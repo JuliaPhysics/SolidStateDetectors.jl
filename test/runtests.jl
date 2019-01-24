7487e72735fecbde5681eb105422830d3dbe56be
Lukas Hauertmann <lhauert@mpp.mpg.de>
2019-01-24 17:15:01 +0100

    Extend checks in runtest.jl for potential simulation

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,34 +26,34 @@ using Test
         @test begin
             global eps_coax
             eps_coax = calculate_electric_potential(det_coax, max_refinements = 1, convergence_limit=5e-5);
-            true
+            !(any(isnan, eps_coax.potential) || any(isinf, eps_coax.potential)) 
         end
         @test begin
             global eps_bege
             eps_bege = calculate_electric_potential(det_bege, max_refinements = 1, convergence_limit=5e-5);
-            true
+            !(any(isnan, eps_bege.potential) || any(isinf, eps_bege.potential)) 
         end
         @test begin
             global eps_ivc
             eps_ivc = calculate_electric_potential(det_ivc, max_refinements = 1, convergence_limit=5e-5);
-            true
+            !(any(isnan, eps_ivc.potential) || any(isinf, eps_ivc.potential)) 
         end
     end
     @testset "Weighting Potential Simulation" begin
         @test begin
             global wps_coax
             wps_coax = calculate_weighting_potential(det_coax, 1, max_refinements = 1, convergence_limit=5e-5);
-            true
+            !(any(isnan, wps_coax.potential) || any(isinf, wps_coax.potential))
         end
         @test begin
             global wps_bege
             wps_bege = calculate_weighting_potential(det_bege, 1, max_refinements = 1, convergence_limit=5e-5);
-            true
+            !(any(isnan, wps_bege.potential) || any(isinf, wps_bege.potential)) 
         end
         @test begin
             global wps_ivc
             wps_ivc = calculate_weighting_potential(det_ivc, 1, max_refinements = 1, convergence_limit=5e-5);
-            true
+            !(any(isnan, wps_ivc.potential) || any(isinf, wps_ivc.potential)) 
         end
     end
 
