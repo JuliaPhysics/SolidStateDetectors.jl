f579d7e0ec3d4849d018bca07348a02d8f22a0d5
schuster <schuster@mpp.mpg.de>
2019-02-11 16:24:13 +0100

    fixed boundary condition look up for weighting potentials in presence of borehole shift

--- src/Config/Config.jl
+++ src/Config/Config.jl
@@ -1,14 +1,14 @@
 """
     abstract type AbstractConfig{T <: AbstractFloat} end
 
-`T`: Type of points or precision. 
+`T`: Type of points or precision.
 """
 abstract type AbstractConfig{T <: AbstractFloat} end
 
 function get_ρ_and_ϵ(pt, config::AbstractConfig{T})::Tuple{T, T} where {T <: AbstractFloat}
     error("User has to define a method `get_ρ_and_ϵ` for `pt::$(typeof(pt))` and `config::$(typeof(config))`.")
 end
-function set_pointtypes_and_fixed_potentials!(pointtypes::Array{PointType, N}, potential::Array{T, N}, grid::Grid{T, N, S}, config::AbstractConfig{T})::Nothing where {T, N, G, S} 
+function set_pointtypes_and_fixed_potentials!(pointtypes::Array{PointType, N}, potential::Array{T, N}, grid::Grid{T, N, S}, config::AbstractConfig{T})::Nothing where {T, N, G, S}
     error("User has to define a method `set_pointtypes_and_fixed_potentials!` for `config::$(typeof(config))`.")
 end
 
@@ -21,13 +21,13 @@ function get_ρ_and_ϵ(pt::Cylindrical{T}, ssd::SolidStateDetector{T})::Tuple{T,
         return ρ, ϵ
     else
         ρ = 0
-        ϵ = ssd.material_environment.ϵ_r 
+        ϵ = ssd.material_environment.ϵ_r
         return ρ, ϵ
     end
 end
-function set_pointtypes_and_fixed_potentials!(pointtypes::Array{PointType, N}, potential::Array{T, N}, 
+function set_pointtypes_and_fixed_potentials!(pointtypes::Array{PointType, N}, potential::Array{T, N},
         grid::Grid{T, N, :Cylindrical}, ssd::SolidStateDetector{T}; weighting_potential_channel_idx::Union{Missing, Int} = missing)::Nothing where {T <: AbstractFloat, N}
-    
+
     channels::Array{Int, 1} = if !ismissing(weighting_potential_channel_idx)
         ssd.grouped_channels[weighting_potential_channel_idx]
     else
@@ -43,18 +43,18 @@ function set_pointtypes_and_fixed_potentials!(pointtypes::Array{PointType, N}, p
             θ::T = axθ[iθ]
             for ir in axes(potential, 1)
                 r::T = axr[ir]
-                pt::Cylindrical{T} = Cylindrical{T}( r, θ, z )              
+                pt::Cylindrical{T} = Cylindrical{T}( r, θ, z )
 
                 if is_boundary_point(ssd, r, θ, z, axr, axθ, axz)
                     pot::T = if ismissing(weighting_potential_channel_idx)
                         get_boundary_value( ssd, r, θ, z, axr)
                     else
-                        in(get_segment_idx(ssd, r, θ, z), channels) ? 1 : 0
+                        ssd.borehole_modulation ? (in(get_segment_idx(ssd, r, θ, z, axr), channels) ? 1 : 0) : (in(get_segment_idx(ssd, r, θ, z), channels) ? 1 : 0)
                     end
                     potential[ ir, iθ, iz ] = pot
                     pointtypes[ ir, iθ, iz ] = zero(PointType)
                 elseif in(pt, ssd)
-                    pointtypes[ ir, iθ, iz ] += pn_junction_bit 
+                    pointtypes[ ir, iθ, iz ] += pn_junction_bit
                 end
 
             end
@@ -62,4 +62,3 @@ function set_pointtypes_and_fixed_potentials!(pointtypes::Array{PointType, N}, p
     end
     nothing
 end
-
