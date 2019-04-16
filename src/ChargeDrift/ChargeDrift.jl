fcb7dddd292d00cc8da93e7371541566720bfc57
Lukas Hauertmann <lhauert@mpp.mpg.de>
2019-04-16 10:41:33 +0200

    adds method `drift_charge` for single starting pos

--- src/ChargeDrift/ChargeDrift.jl
+++ src/ChargeDrift/ChargeDrift.jl
@@ -26,3 +26,16 @@ function drift_charges( detector::SolidStateDetector{T}, grid::Grid{T, 3},
     return drift_paths 
 end
 
+function drift_charge( detector::SolidStateDetector{T}, grid::Grid{T, 3},
+                       starting_point::CartesianPoint{T}, 
+                       velocity_field_e::Interpolations.Extrapolation{SVector{3, Float64}, 3},
+                       velocity_field_h::Interpolations.Extrapolation{SVector{3, Float64}, 3}; 
+                       Δt::T = T(1f-9), n_steps::Int = 2000)::Vector{DriftPath{T}} where {T <: SSDFloat}
+
+    drift_path_e::Vector{CartesianPoint{T}} = Vector{CartesianPoint{T}}(undef, n_steps)
+    drift_path_h::Vector{CartesianPoint{T}} = Vector{CartesianPoint{T}}(undef, n_steps)
+    drift_charge!(drift_path_e, detector, grid, starting_point, Δt, velocity_field_e)
+    drift_charge!(drift_path_h, detector, grid, starting_point, Δt, velocity_field_h)
+    return DriftPath{T}( drift_path_e, drift_path_h )
+end
+
