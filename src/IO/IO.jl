2fc84fcaada950599f824e9b6f3f0e86e6cbf4b8
Lukas Hauertmann <lhauert@mpp.mpg.de>
2019-10-22 17:36:11 +0200

    Removed convert method since it is now defined in another package we are already using

--- src/IO/IO.jl
+++ src/IO/IO.jl
@@ -22,7 +22,7 @@ Base.convert(T::Type{NamedTuple}, x::Dict) = T(x)
 function Dict(nt::NamedTuple)
     JSON.parse(nt.dict_json_string)
 end
-Base.convert(T::Type{Dict}, x::NamedTuple) = T(x)
+# Base.convert(T::Type{Dict}, x::NamedTuple) = T(x) # is now defined in NamedTupleTools-Package
 
 
 
