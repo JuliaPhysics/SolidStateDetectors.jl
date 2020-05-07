4bc17e44cd34dba3125017bc0c6d1cde6e31305d
Lukas Hauertmann <lhauert@mpp.mpg.de>
2020-05-07 17:32:50 +0200

    Add an ID to `ArbitraryDriftModificationVolume`

--- src/SolidStateDetector/TransitionLayer.jl
+++ src/SolidStateDetector/TransitionLayer.jl
@@ -17,17 +17,25 @@ end
 
 struct ArbitraryDriftModificationVolume{T} <: AbstractVirtualVolume{T}
     name::String
+    id::Int
     geometry::AbstractGeometry{T}
 end
 
 function modulate_driftvector(sv::CartesianVector{T}, cp::CartesianPoint{T}, tl::ArbitraryDriftModificationVolume{T})::CartesianVector{T} where {T <: SSDFloat}
-    error("This function needs to be overwritten by the user.")
+    modulate_driftvector(sv, cp, tl, Val{tl.id})
+end
+function modulate_driftvector(sv::CartesianVector{T}, cp::CartesianPoint{T}, tl::ArbitraryDriftModificationVolume{T}, ::Type{Val{id}})::CartesianVector{T} where {T <: SSDFloat, id}
+    error("""
+        This function needs to be overwritten by the user. Use `::Type{Val{<id>}}` for the last argument. 
+        <id> is the corresponding id specified in the json-config file of the detector. 
+    """)
 end
 
 
 function ArbitraryDriftModificationVolume{T}(dict::Dict, inputunit_dict::Dict{String,Unitful.Units}) where T <: SSDFloat
     n = haskey(dict, "name") ? dict["name"] : "external part"
     g = Geometry(T, dict["geometry"], inputunit_dict)
-    return ArbitraryDriftModificationVolume{T}(n, g)
+    id = Int(dict["id"])
+    return ArbitraryDriftModificationVolume{T}(n, id, g)
 end
 
