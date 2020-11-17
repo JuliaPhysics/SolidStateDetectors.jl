7aaab520117a065f2219b2105eec41f122c01314
Martin Schuster <schuster@mpp.mpg.de>
2020-11-17 20:26:27 +0100

    Enabled electric field plot for documentation

--- docs/src/tutorial_lit.jl
+++ docs/src/tutorial_lit.jl
@@ -1,4 +1,4 @@
-# # Example 1: Inverted Coax Detector 
+# # Example 1: Inverted Coax Detector
 
 using Plots
 using SolidStateDetectors
@@ -11,7 +11,7 @@ plot(simulation.detector, size = (700, 700))
 
 # One can also have a look at how the initial conditions look like on the grid (its starts with a very coarse grid):
 
-apply_initial_state!(simulation, ElectricPotential) # optional 
+apply_initial_state!(simulation, ElectricPotential) # optional
 plot(
     plot(simulation.electric_potential), # initial electric potential (boundary conditions)
     plot(simulation.point_types), # map of different point types: fixed point / inside or outside detector volume / depleted/undepleted
@@ -21,8 +21,8 @@ plot(
 )
 
 # Next, calculate the electric potential:
- 
-calculate_electric_potential!( simulation, 
+
+calculate_electric_potential!( simulation,
                                max_refinements = 3)
 
 plot(
@@ -38,26 +38,26 @@ plot(
 
 get_active_volume(simulation.point_types) # approximation (sum of the volume of cells marked as depleted)
 
- 
- 
+
+
 # ## Partially depleted detectors
- 
+
 # SolidStateDetectors.jl can also calculate the electric potential of a partially depleted detector:
- 
+
 detector_undep = deepcopy(simulation.detector)
 detector_undep.contacts[end].potential = 500; # V  <-- Bias Voltage of Mantle
 
 simulation_undep = Simulation(detector_undep);
 
-calculate_electric_potential!( simulation_undep, 
-                               depletion_handling = true, 
+calculate_electric_potential!( simulation_undep,
+                               depletion_handling = true,
                                convergence_limit=1e-6,
-                               max_refinements = 3, 
+                               max_refinements = 3,
                                verbose = false)
 
 plot(
-    plot(simulation_undep.electric_potential), 
-    plot(simulation_undep.point_types), 
+    plot(simulation_undep.electric_potential),
+    plot(simulation_undep.point_types),
     layout = (1, 2), size = (800, 700)
 )
 
@@ -74,7 +74,8 @@ println("Undepleted: ", get_active_volume(simulation_undep.point_types));
 
 calculate_electric_field!(simulation, n_points_in_φ = 72)
 
-# plot_electric_field(simulation, size = (350, 500))
+plot(simulation.electric_field, φ = 0.0, size = (350, 500))
+plot_electric_fieldlines!(simulation, φ = 0.0)
 
 # ## Drift field calculation
 
@@ -87,17 +88,17 @@ calculate_electric_field!(simulation, n_points_in_φ = 72)
 # Set the charge drift model of the simulation:
 
 charge_drift_model = ADLChargeDriftModel()
-set_charge_drift_model!(simulation, charge_drift_model) 
+set_charge_drift_model!(simulation, charge_drift_model)
 
- 
+
 # And apply the charge drift model to the electric field:
- 
+
 calculate_drift_fields!(simulation)
 
 # Now, let's create an "random" (multiside) event:
 
-starting_positions = [ CylindricalPoint{T}( 0.020, deg2rad(10), 0.015 ), 
-                       CylindricalPoint{T}( 0.015, deg2rad(20), 0.045 ), 
+starting_positions = [ CylindricalPoint{T}( 0.020, deg2rad(10), 0.015 ),
+                       CylindricalPoint{T}( 0.015, deg2rad(20), 0.045 ),
                        CylindricalPoint{T}( 0.025, deg2rad(30), 0.025 ) ]
 energy_depos = T[1460, 609, 1000] * u"keV" # are needed later in the signal generation
 
@@ -117,7 +118,7 @@ for contact in simulation.detector.contacts
     calculate_weighting_potential!(simulation, contact.id, max_refinements = 3, n_points_in_φ = 2, verbose = false)
 end
 
-plot(  
+plot(
     plot(simulation.weighting_potentials[1]),
     plot(simulation.weighting_potentials[2]),
     size = (900, 700)
@@ -126,10 +127,10 @@ plot(
 # ## Detector waveform generation
 
 # ### Single-event simulation
- 
+
 # Given an interaction at an arbitrary point in the detector, we can now simulate the charge drift and the resulting detector charge signals (e.g. at the point contact):
 
 simulate!(event, simulation) # drift_charges + signal generation of all channels
 
-p_pc_signal = plot( event.waveforms[1], lw = 1.5, xlims = (0, 1100), xlabel = "Time / ns", 
+p_pc_signal = plot( event.waveforms[1], lw = 1.5, xlims = (0, 1100), xlabel = "Time / ns",
                     legend = false, tickfontsize = 12, ylabel = "Energy / eV", guidefontsize = 14)
