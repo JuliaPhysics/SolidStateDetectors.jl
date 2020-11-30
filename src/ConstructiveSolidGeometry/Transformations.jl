8f774c294901c83975803492f4edf05e78429996
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2020-11-30 20:17:44 +0100

    revert "restructure repeated transformations"
    This reverts commit 76552a7b60778fa04fd0400df03e49d7995a55de.

--- src/ConstructiveSolidGeometry/Transformations.jl
+++ src/ConstructiveSolidGeometry/Transformations.jl
@@ -1,20 +1,21 @@
 struct ScaledGeometry{T,P<:AbstractGeometry{T}} <: AbstractGeometry{T}
     p::P
     inv_s::SVector{3,T}
-    ScaledGeometry(p::AbstractGeometry{T}, s::SVector{3,T}) where {T} = new{T,typeof(p)}(p, inv.(s))
-    ScaledGeometry(g::ScaledGeometry{T}, s::SVector{3,T}) where {T} = new{T,typeof(g.p)}(g.p, g.inv_s .* inv.(s))
+    ScaledGeometry(p::P, s::SVector{3,T}) where {T,P} = new{T,P}(p, inv.(s))
 end
 in(p::CartesianPoint, g::ScaledGeometry) = in(CartesianPoint(g.inv_s .* p), g.p)
 scale(g::AbstractGeometry, s::SVector{3,T}) where {T} = ScaledGeometry(g, s)
+scale(g::ScaledGeometry, s::SVector{3,T}) where {T} = ScaledGeometry(g.p, inv.(g.inv_s) .* s)
 
 struct RotatedGeometry{T,P<:AbstractGeometry{T},RT} <: AbstractGeometry{T}
     p::P
     inv_r::RotMatrix{3,RT,9}
     RotatedGeometry(p::AbstractGeometry{T}, r::RotMatrix{3,RT,9}) where {T,RT} = new{T,typeof(p),RT}(p, inv(r))
-    RotatedGeometry(g::RotatedGeometry{T}, r::RotMatrix{3,RT,9}) where {T,RT} = new{T,typeof(g.p),RT}(g.p, g.inv_r * inv(r))
 end
 in(p::CartesianPoint, g::RotatedGeometry) = in(g.inv_r * p, g.p)
 rotate(g::AbstractGeometry{T}, r::RotMatrix{3,RT,9}) where {T,RT} = RotatedGeometry(g, r)
+rotate(g::RotatedGeometry{T,<:Any,RT}, r::RotMatrix{3,RT,9}) where {T,RT} = RotatedGeometry(g.p, r * inv(g.inv_r))
+
 
 struct TranslatedGeometry{T,P<:AbstractGeometry{T}} <: AbstractGeometry{T}
     p::P
