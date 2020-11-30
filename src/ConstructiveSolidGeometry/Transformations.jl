214249d61ec08bdbc2fe0133108104e16244d0b7
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2020-11-30 16:04:17 +0100

    rename StretchedGeometry to ScaledGeometry

--- src/ConstructiveSolidGeometry/Transformations.jl
+++ src/ConstructiveSolidGeometry/Transformations.jl
@@ -1,20 +1,20 @@
-struct StretchedGeometry{T,P<:AbstractGeometry{T}} <: AbstractGeometry{T}
+struct ScaledGeometry{T,P<:AbstractGeometry{T}} <: AbstractGeometry{T}
     p::P
     inv_s::SVector{3,T}
-    StretchedGeometry(p::P, s::SVector{3,T}) where {T,P} = new{T,P}(p, inv.(s))
+    ScaledGeometry(p::P, s::SVector{3,T}) where {T,P} = new{T,P}(p, inv.(s))
 end
-in(p::CartesianPoint, g::StretchedGeometry) = in(CartesianPoint(g.inv_s .* p), g.p)
-stretch(g::AbstractGeometry, s::SVector{3,T}) where {T} = StretchedGeometry(g, s)
-stretch(g::StretchedGeometry, s::SVector{3,T}) where {T} = StretchedGeometry(g.p, g.inv_s .* inv.(s))
+in(p::CartesianPoint, g::ScaledGeometry) = in(CartesianPoint(g.inv_s .* p), g.p)
+scale(g::AbstractGeometry, s::SVector{3,T}) where {T} = ScaledGeometry(g, s)
+scale(g::ScaledGeometry, s::SVector{3,T}) where {T} = ScaledGeometry(g.p, g.inv_s .* inv.(s))
 
 struct RotatedGeometry{T,P<:AbstractGeometry{T},RT} <: AbstractGeometry{T}
     p::P
     inv_r::RotMatrix{3,RT,9}
-    RotatedGeometry(p::AbstractGeometry{T}, r::RotMatrix{3,RT,9}) where {T,RT} = new{T,typeof(p),RT}(p, r)
+    RotatedGeometry(p::AbstractGeometry{T}, r::RotMatrix{3,RT,9}) where {T,RT} = new{T,typeof(p),RT}(p, inv(r))
 end
 in(p::CartesianPoint, g::RotatedGeometry) = in(g.inv_r * p, g.p)
 rotate(g::AbstractGeometry{T}, r::RotMatrix{3,RT,9}) where {T,RT} = RotatedGeometry(g, r)
-rotate(g::RotatedGeometry{T,<:Any,RT}, r::RotMatrix{3,RT,9}) where {T,RT} = RotatedGeometry(g.p, r * gp.r)
+rotate(g::RotatedGeometry{T,<:Any,RT}, r::RotMatrix{3,RT,9}) where {T,RT} = RotatedGeometry{T,typeof(g),RT}(g.p, inv(r) * gp.r)
 
 
 struct TranslatedGeometry{T,P<:AbstractGeometry{T}} <: AbstractGeometry{T}
