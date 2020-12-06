cf08420bbfbca95fb6496fceae21ee9d6e157414
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2020-12-06 20:35:28 +0100

    update plot recipes for AbstractGeometry

--- src/ConstructiveSolidGeometry/plot_recipes.jl
+++ src/ConstructiveSolidGeometry/plot_recipes.jl
@@ -1,4 +1,4 @@
-@recipe function f(c::AbstractGeometry{T}) where {T}
+@recipe function f(g::AbstractGeometry{T}) where {T}
     
     seriescolor --> :orange
     linewidth --> 2
@@ -7,14 +7,19 @@
         label --> "Geometry"
         []
     end
-
-    for points in get_plot_points(c)
-        @series begin
-            label := ""
-            points
+    
+    pos, _ = get_decomposed_volumes(g)
+    
+    for p in pos
+        for points in get_plot_points(p)
+            @series begin
+                label := ""
+                points
+            end
         end
     end
 end
+
 
 @recipe function f(points::Vector{CartesianPoint{T}}) where {T}
     map(p -> p.x, points), map(p -> p.y, points), map(p -> p.z, points)
