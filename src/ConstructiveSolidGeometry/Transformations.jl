ef44dc1ae65ccf5429cfbcdb5b6ec1a3e216a79b
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2020-12-06 18:32:11 +0100

    extend in()-method for CylindricalPoint

--- src/ConstructiveSolidGeometry/Transformations.jl
+++ src/ConstructiveSolidGeometry/Transformations.jl
@@ -3,7 +3,8 @@ struct ScaledGeometry{T,P<:AbstractGeometry{T}} <: AbstractGeometry{T}
     inv_s::SVector{3,T}
     ScaledGeometry(p::P, s::SVector{3,T}) where {T,P} = new{T,P}(p, inv.(s))
 end
-in(p::CartesianPoint, g::ScaledGeometry) = in(CartesianPoint(g.inv_s .* p), g.p)
+in(p::CartesianPoint, g::ScaledGeometry) = in(CartesianPoint(p .* g.inv_s), g.p)
+in(p::CylindricalPoint, g::ScaledGeometry) = in(CartesianPoint(p), g)
 scale(g::AbstractGeometry{T}, s::SVector{3,T}) where {T} = ScaledGeometry(g, s)
 scale(g::ScaledGeometry{T}, s::SVector{3,T}) where {T} = ScaledGeometry(g.p, inv.(g.inv_s) .* s)
 #(*)(g::AbstractGeometry{T}, s::SVector{3,T}) where {T} = scale(g, s)
@@ -16,9 +17,10 @@ struct RotatedGeometry{T,P<:AbstractGeometry{T},RT} <: AbstractGeometry{T}
     RotatedGeometry(p::AbstractGeometry{T}, r::RotMatrix{3,RT,9}) where {T,RT} = new{T,typeof(p),RT}(p, inv(r))
 end
 in(p::CartesianPoint, g::RotatedGeometry) = in(g.inv_r * p, g.p)
+in(p::CylindricalPoint, g::RotatedGeometry) = in(CartesianPoint(p), g)
 rotate(g::AbstractGeometry{T}, r::RotMatrix{3,RT,9}) where {T,RT} = RotatedGeometry(g, r)
 rotate(g::RotatedGeometry{T,<:Any,RT}, r::RotMatrix{3,RT,9}) where {T,RT} = RotatedGeometry(g.p, r * inv(g.inv_r))
-#(*)(g::AbstractGeometry{T}, r::RotMatrix{3,RT,9}) where {T,RT} = rotate(g, r)
+(*)(g::AbstractGeometry{T}, r::RotMatrix{3,RT,9}) where {T,RT} = rotate(g, r)
 get_plot_points(rg::RotatedGeometry{T}) where {T} = rotate!(get_plot_points(rg.p), inv(rg.inv_r))
 
 
@@ -27,7 +29,8 @@ struct TranslatedGeometry{T,P<:AbstractGeometry{T}} <: AbstractGeometry{T}
     t::CartesianVector{T}
 end
 in(p::CartesianPoint, g::TranslatedGeometry) = in(p - g.t, g.p)
+in(p::CylindricalPoint, g::TranslatedGeometry) = in(CartesianPoint(p), g)
 translate(g::AbstractGeometry{T}, t::CartesianVector{T}) where {T} = TranslatedGeometry(g, t)
 translate(g::TranslatedGeometry{T}, t::CartesianVector{T}) where {T} = TranslatedGeometry(g.p, g.t + t)
-#(+)(g::AbstractGeometry{T}, t::CartesianVector{T}) where {T} = translate(g, t)
+(+)(g::AbstractGeometry{T}, t::CartesianVector{T}) where {T} = translate(g, t)
 get_plot_points(tg::TranslatedGeometry{T}) where {T} = translate!(get_plot_points(tg.p), tg.t)
