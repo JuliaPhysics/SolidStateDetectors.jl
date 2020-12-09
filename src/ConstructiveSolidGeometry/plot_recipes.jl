c43ac9ec67198194ddb73e2a6d4fa5eee224e774
hervasa2 <hervasa2@live.unc.edu>
2020-12-09 19:35:29 +0100

    corr from merge

--- src/ConstructiveSolidGeometry/plot_recipes.jl
+++ src/ConstructiveSolidGeometry/plot_recipes.jl
@@ -1,4 +1,4 @@
-@recipe function f(c::AbstractGeometry{T}; SSD_style = :wireframe) where {T}
+@recipe function f(g::AbstractGeometry{T}; SSD_style = :wireframe) where {T}
 
     seriescolor --> :orange
 
@@ -12,14 +12,15 @@
         pos, _ = get_decomposed_volumes(g)
         for p in pos
             for points in get_plot_points(p)
-            @series begin
-                label := ""
-                points
+                @series begin
+                    label := ""
+                    points
+                end
             end
         end
     elseif SSD_style == :surface
         linewidth --> 0.1
-        for mesh in get_plot_meshes(c)
+        for mesh in get_plot_meshes(g)
             @series begin
                 label := ""
                 mesh
