660e5ee5342d173a8dac330244988c124a2073e3
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-04-14 16:57:05 +0200

    Update parsing of CSG intervals

--- src/ConstructiveSolidGeometry/IO.jl
+++ src/ConstructiveSolidGeometry/IO.jl
@@ -16,27 +16,34 @@ volume_primitive_dict = Dict{String, Any}(
 @inline _parse_value(::Type{T}, x::Real, unit::Unitful.Units) where {T} = to_internal_units(T(x) * unit)
 @inline _parse_value(::Type{T}, s::String, ::Unitful.Units) where {T} = to_internal_units(T(uparse(s)))
 
-# parses dictionary entries of type {"from": ..., "to": ... } to respective AbstractFloat/Interval
-function _parse_interval_from_to(::Type{T}, dict::Union{Dict{String,Any}, Dict{Any,Any}}, unit::Unitful.Units) where {T}
+# parses dictionary entries of type {"from": ..., "to": ... } to a Tuple of the interval boundaries
+function _parse_interval_from_to(::Type{T}, dict::Union{Dict{String,Any}, Dict{Any,Any}}, unit::Unitful.Units)::Tuple{T,T} where {T}
     To::T = _parse_value(T, dict["to"], unit)
     From::T = _parse_value(T, dict["from"], unit)
-    (To == -From) ? To : From..To
+    @assert From <= To "Entry 'from' should be smaller than entry 'to' in $(dict)."
+    (From, To)
 end
 
 # parses dictionary entries of type Real, String or {"from": ..., "to": ... } to respective AbstractFloat/Interval
-@inline _parse_interval(::Type{T}, x::Union{Real, String}, unit::Unitful.Units) where {T} = _parse_value(T, x, unit)
-function _parse_interval(::Type{T}, dict::Union{Dict{String,Any}, Dict{Any,Any}}, unit::Unitful.Units) where {T}
-    @assert haskey(dict, "from") && haskey(dict, "to") "Please specify 'from' and 'to' in $(dict)"
-    _parse_interval_from_to(T, dict, unit)
+@inline _parse_radial_interval(::Type{T}, x::Union{Real, String}, unit::Unitful.Units) where {T} = _parse_value(T, x, unit)
+function _parse_radial_interval(::Type{T}, dict::Union{Dict{String,Any}, Dict{Any,Any}}, unit::Unitful.Units) where {T}
+    @assert haskey(dict, "from") && haskey(dict, "to") "Please specify 'from' and 'to' in $(dict)."
+    From::T, To::T = _parse_interval_from_to(T, dict, unit)
+    @assert From >= 0 && To >= 0 "Entries 'from' and 'to' of radial $(dict) should be non-zero."
+    From == 0 ? To : From..To
 end
 
-#parses dictionary entry for heights to respective AbstractFloat/Interval (divides by 2 if it is a single value)
-@inline _parse_interval_height(::Type{T}, x::Union{Real, String}, unit::Unitful.Units) where {T} = _parse_value(T, x, unit)/2
-@inline _parse_interval_height(::Type{T}, dict::Union{Dict{String,Any}, Dict{Any,Any}}, unit::Unitful.Units) where {T} = _parse_interval(T, dict, unit)
+# parses dictionary entries of type Real, String or {"from": ..., "to": ... } to respective AbstractFloat/Interval
+@inline _parse_linear_interval(::Type{T}, x::Union{Real, String}, unit::Unitful.Units) where {T} = _parse_value(T, x, unit)/2
+function _parse_linear_interval(::Type{T}, dict::Union{Dict{String,Any}, Dict{Any,Any}}, unit::Unitful.Units) where {T}
+    @assert haskey(dict, "from") && haskey(dict, "to") "Please specify 'from' and 'to' in $(dict)."
+    From::T, To::T = _parse_interval_from_to(T, dict, unit)
+    To == -From ? To : From..To
+end
 
 
 # parses dictionary entry for φ-interval that has {"from" ..., "to": ...} to the respective Nothing/Interval
-function _parse_interval_φ(::Type{T}, dict::Union{Dict{String,Any}, Dict{Any,Any}}, unit::Unitful.Units) where {T}
+function _parse_angular_interval(::Type{T}, dict::Union{Dict{String,Any}, Dict{Any,Any}}, unit::Unitful.Units) where {T}
     φTo::T = _parse_value(T, dict["to"], unit)
     φFrom::T = _parse_value(T, dict["from"], unit)
     if abs(rem2pi(φFrom - φTo, RoundNearest)) > 2*eps(T)
@@ -55,13 +62,13 @@ function parse_r_of_primitive(::Type{T}, dict::Union{Dict{String,Any}, Dict{Any,
     dictr = dict["r"]
     # "r" : {"bottom": {"from": ..., "to": ...}, "top": {"from": ..., "to": ...}}
     if haskey(dictr, "bottom") && haskey(dictr, "top")
-        bottom = _parse_interval(T, dictr["bottom"], unit)
-        top = _parse_interval(T, dictr["top"], unit)
+        bottom = _parse_radial_interval(T, dictr["bottom"], unit)
+        top = _parse_radial_interval(T, dictr["top"], unit)
         # bottom and top need to be same type for Cone
         typeof(bottom) == typeof(top) ? (bottom, top) : _extend_number_to_zero_interval.((bottom, top))
     # "r" : {"from": ..., "to": ...}
     else
-        _parse_interval(T, dictr, unit)
+        _parse_radial_interval(T, dictr, unit)
     end
 end
 
@@ -72,20 +79,20 @@ function parse_φ_of_primitive(::Type{T}, dict::Union{Dict{String,Any}, Dict{Any
         @info "No 'phi' is specified in the '$(dict["type"])'. Assuming 'phi' to go from 0 to 360°."
         nothing
     else #haskey(dict, "phi")
-        _parse_interval_φ(T, dict["phi"], unit)
+        _parse_angular_interval(T, dict["phi"], unit)
     end
 end
 
 # converts either "h" or "z" to the respective AbstractFloat/Interval
 function parse_height_of_primitive(::Type{T}, dict::Union{Dict{String,Any}, Dict{Any,Any}}, unit::Unitful.Units) where {T}
     @assert haskey(dict,"h") || haskey(dict,"z") "Please specify 'h' or 'z' of the '$(dict["type"])'."
-    haskey(dict,"h") ? _parse_interval_height(T, dict["h"], unit) : _parse_interval_height(T, dict["z"], unit)
+    haskey(dict,"h") ? _parse_linear_interval(T, dict["h"], unit) : _parse_linear_interval(T, dict["z"], unit)
 end
 
-# converts the content of the dictionary to respective AbstractFloat/Interval. Numbers will not be divided by 2!
+# converts the content of the dictionary to respective AbstractFloat/Interval.
 function parse_interval_of_primitive(::Type{T}, s::String, dict::Union{Dict{String,Any}, Dict{Any,Any}}, unit::Unitful.Units) where {T}
     @assert haskey(dict, s) "Please specify '$(s)' of the '$(dict["type"])'."
-    _parse_interval(T, dict[s], unit)
+    _parse_linear_interval(T, dict[s], unit)
 end
 
 
