46a3ad2d30e8f700ddf5a709761962960da5f9ea
hervasa2 <hervasa2@live.unc.edu>
2021-04-22 22:36:49 +0200

    Surf mods

--- src/ConstructiveSolidGeometry/SurfacePrimitives/ToroidalAnnulus.jl
+++ src/ConstructiveSolidGeometry/SurfacePrimitives/ToroidalAnnulus.jl
@@ -1,33 +1,42 @@
-struct ToroidalAnnulus{T,TR,TB,TP,TT} <: AbstractSurfacePrimitive{T}
+struct ToroidalAnnulus{T,TR,TB,TP,TT,TZ} <: AbstractSurfacePrimitive{T}
     r_torus::TR
     r_tube::TB
     φ::TP
     θ::TT
+    z::TZ
     function ToroidalAnnulus( ::Type{T},
                    r_torus::T,
                    r_tube::Union{T, <:AbstractInterval{T}},
                    φ::T,
-                   θ::Union{Nothing, <:AbstractInterval{T}}) where {T}
-        new{T,T,typeof(r_tube),T,typeof(θ)}(r_torus, r_tube, φ, θ)
+                   θ::Union{Nothing, <:AbstractInterval{T}},
+                   z::T) where {T}
+        new{T,T,typeof(r_tube),T,typeof(θ),T}(r_torus, r_tube, φ, θ, z)
     end
 end
 
 #Constructors
-ToroidalAnnulus(t::Torus{T}; φ = 0) where {T} = ToroidalAnnulus( T, t.r_torus, t.r_tube, T(mod(φ,2π)), t.θ)
+ToroidalAnnulus(t::Torus{T}; φ = 0) where {T} = ToroidalAnnulus( T, t.r_torus, t.r_tube, T(mod(φ,2π)), t.θ, t.z)
 
-function ToroidalAnnulus(;r_torus = 1, r_tubeMin = 0, r_tubeMax = 1, φ = 0, θMin = 0, θMax = 2π)
-    T = float(promote_type(typeof.((r_torus, r_tubeMin, r_tubeMax, φ, θMin, θMax))...))
+function ToroidalAnnulus(;r_torus = 1, r_tubeMin = 0, r_tubeMax = 1, φ = 0, θMin = 0, θMax = 2π, z = 0)
+    T = float(promote_type(typeof.((r_torus, r_tubeMin, r_tubeMax, φ, θMin, θMax, z))...))
     r_tube = r_tubeMin == 0 ? T(r_tubeMax) : T(r_tubeMin)..T(r_tubeMax)
     θ = mod(T(θMax) - T(θMin), T(2π)) == 0 ? nothing : T(θMin)..T(θMax)
-    ToroidalAnnulus( T, T(r_torus), r_tube, T(φ), θ)
+    ToroidalAnnulus( T, T(r_torus), r_tube, T(φ), θ, T(z))
 end
-ToroidalAnnulus(r_torus, r_tubeMin, r_tubeMax, φ, θMin, θMax) = ToroidalAnnulus(;r_torus = r_torus, r_tubeMin = r_tubeMin, r_tubeMax = r_tubeMax, φ = φ, θMin = θMin, θMax = θMax)
+
+ToroidalAnnulus(r_torus, r_tubeMin, r_tubeMax, φ, θMin, θMax, z) = ToroidalAnnulus(;r_torus = r_torus, r_tubeMin = r_tubeMin, r_tubeMax = r_tubeMax, φ = φ, θMin = θMin, θMax = θMax, z = z)
+
+in(p::PlanarPoint, t::ToroidalAnnulus{<:Any, <:Any, <:Any, <:Any, Nothing}) =
+    _in_planar_r(p - PlanarPoint(t.r_torus, t.z), t.r_tube)
+
+in(p::PlanarPoint, t::ToroidalAnnulus{<:Any, <:Any, <:Any, <:Any, <:AbstractInterval}) =
+    _in_planar_r(p - PlanarPoint(t.r_torus, t.z), t.r_tube) && _in_planar_α(p - PlanarPoint(t.r_torus, t.z), t.θ)
 
 in(p::AbstractCoordinatePoint, t::ToroidalAnnulus{<:Any, <:Any, <:Any, <:Any, Nothing}) =
-    _in_torr_r_tube(p, t.r_torus, t.r_tube) && _eq_φ(p, t.φ)
+    _in_torr_r_tube(p, t.r_torus, t.r_tube, t.z) && _eq_φ(p, t.φ)
 
 in(p::AbstractCoordinatePoint, t::ToroidalAnnulus{<:Any, <:Any, <:Any, <:Any, <:AbstractInterval}) =
-    _in_torr_r_tube(p, t.r_torus, t.r_tube) && _eq_φ(p, t.φ) && _in_torr_θ(p, t.r_torus, t.θ)
+    _in_torr_r_tube(p, t.r_torus, t.r_tube, t.z) && _eq_φ(p, t.φ) && _in_torr_θ(p, t.r_torus, t.θ, t.z)
 
 get_r_tube_limits(t::ToroidalAnnulus{T}) where {T} =
     (_left_radial_interval(t.r_tube),_right_radial_interval(t.r_tube))
@@ -39,7 +48,7 @@ function sample(t::ToroidalAnnulus{T}, step::Real) where {T}
     r_tubeMin::T, r_tubeMax::T = get_r_tube_limits(t)
     θMin::T, θMax::T, _ = get_θ_limits(t)
     samples = [
-        CylindricalPoint{T}(t.r_torus+r_tube*cos(θ),t.φ,r_tube*sin(θ))
+        CylindricalPoint{T}(t.r_torus+r_tube*cos(θ),t.φ,r_tube*sin(θ)+t.z)
         for r_tube in r_tubeMin:step:r_tubeMax
         for θ in (r_tube == 0 ? θMin : θMin:step/r_tube:θMax)
     ]
@@ -49,7 +58,7 @@ function sample(t::ToroidalAnnulus{T}, Nsamps::NTuple{3,Int}) where {T}
     r_tubeMin::T, r_tubeMax::T = get_r_tube_limits(t)
     θMin::T, θMax::T, _ = get_θ_limits(t)
     samples = [
-        CylindricalPoint{T}(t.r_torus+r_tube*cos(θ),t.φ,r_tube*sin(θ))
+        CylindricalPoint{T}(t.r_torus+r_tube*cos(θ),t.φ,r_tube*sin(θ)+t.z)
         for r_tube in (Nsamps[1] ≤ 1 ? r_tubeMin : range(r_tubeMin, r_tubeMax, length = Nsamps[1]))
         for θ in (Nsamps[3] ≤ 1 ? θMin : range(θMin, θMax, length = Nsamps[3]))
     ]
