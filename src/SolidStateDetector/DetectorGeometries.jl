3f5c8d86301926424fe67a326f0505ceb76abfb2
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-06-09 15:11:24 +0200

    Add paint function for `Polygon`

--- src/SolidStateDetector/DetectorGeometries.jl
+++ src/SolidStateDetector/DetectorGeometries.jl
@@ -416,6 +416,40 @@ function set_pointtypes_and_fixed_potentials!(pointtypes::Array{PointType, N}, p
     nothing
 end
 
+function paint!(pointtypes, potential, face::ConstructiveSolidGeometry.Polygon, geometry, pot_value, grid)
+    ticks = CartesianTicksTuple(grid)
+    t_idx_r1, t_idx_r2, proj = ConstructiveSolidGeometry.get_2d_grid_ticks_and_proj(face, ticks)
+    t1, t2 = if proj == Val{:xy}() 
+        ticks[1], ticks[2]
+    elseif proj == Val{:xz}() 
+        ticks[1], ticks[3]
+    else
+        ticks[2], ticks[3]
+    end
+    plane = ConstructiveSolidGeometry.Plane(face)
+    for i2 in t_idx_r2
+        for i1 in t_idx_r1
+            pt = ConstructiveSolidGeometry.evaluate(plane, t1[i1], t2[i2], proj)
+            if pt in geometry
+                if proj == Val{:xy}() 
+                    i3 = searchsortednearest(ticks[3], pt[3])
+                    pointtypes[i1, i2, i3] = zero(PointType)
+                    potential[i1, i2, i3] = pot_value
+                elseif proj == Val{:xz}() 
+                    i3 = searchsortednearest(ticks[2], pt[2])
+                    pointtypes[i1, i3, i2] = zero(PointType)
+                    potential[i1, i3, i2] = pot_value
+                else
+                    i3 = searchsortednearest(ticks[1], pt[1])
+                    pointtypes[i3, i1, i2] = zero(PointType)
+                    potential[i3, i1, i2] = pot_value
+                end
+            end
+        end
+    end 
+    nothing
+end
+
 function json_to_dict(inputfile::String)::Dict
     parsed_json_file = Dict()
     open(inputfile,"r") do f
