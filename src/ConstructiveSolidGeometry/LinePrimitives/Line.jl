86ed307bad64e2a090cfb056047a1a0bc6c7fba7
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-06-17 14:43:05 +0200

    Add a method `_transform_into_object_coordinate_system` for `Line`

--- src/ConstructiveSolidGeometry/LinePrimitives/Line.jl
+++ src/ConstructiveSolidGeometry/LinePrimitives/Line.jl
@@ -4,3 +4,9 @@ struct Line{T} <: AbstractLinePrimitive{T}
 end
 
 distance(pt::CartesianPoint, l::Line) = norm((pt - l.origin) × l.direction) / norm(l.direction)
+
+function _transform_into_object_coordinate_system(l::Line{T}, p::AbstractPrimitive) where {T}
+    origin = _transform_into_object_coordinate_system(l.origin, p) 
+    direction = _transform_into_object_coordinate_system(CartesianPoint{T}(l.direction), p)
+    Line{T}( origin, direction )  
+end
