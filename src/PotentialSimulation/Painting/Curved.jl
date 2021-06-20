7026964d4c28bc16e8c6829e954c13cb4c1fe126
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-06-20 10:07:09 +0200

    Add `paint!` method for `Curved` surfaces on a cylindrical grid

--- src/PotentialSimulation/Painting/Curved.jl
+++ src/PotentialSimulation/Painting/Curved.jl
@@ -48,37 +48,57 @@ function paint!(pointtypes, potential, face::AbstractCurvedSurfacePrimitive{T}, 
 end
 
 
-# function paint!(pointtypes, potential, face::ConstructiveSolidGeometry.CylinderMantle, geometry, pot_value, grid::CartesianGrid)
-#     ticks = TicksTuple(grid)
-#     t_idx_r1, t_idx_r2, proj = ConstructiveSolidGeometry.get_2d_grid_ticks_and_proj(face, ticks)
-#     t1, t2 = if proj == Val{:xy}() 
-#         ticks[1], ticks[2]
-#     elseif proj == Val{:xz}() 
-#         ticks[1], ticks[3]
-#     else
-#         ticks[2], ticks[3]
-#     end
-#     for i2 in t_idx_r2
-#         for i1 in t_idx_r1
-#             pt1, pt2 = ConstructiveSolidGeometry.evaluate(face, t1[i1], t2[i2], proj)
-#             for pt in (pt1, pt2)
-#                 if pt in geometry
-#                     if proj == Val{:xy}() 
-#                         i3 = searchsortednearest(ticks[3], pt[3])
-#                         pointtypes[i1, i2, i3] = zero(PointType)
-#                         potential[i1, i2, i3] = pot_value
-#                     elseif proj == Val{:xz}() 
-#                         i3 = searchsortednearest(ticks[2], pt[2])
-#                         pointtypes[i1, i3, i2] = zero(PointType)
-#                         potential[i1, i3, i2] = pot_value
-#                     else
-#                         i3 = searchsortednearest(ticks[1], pt[1])
-#                         pointtypes[i3, i1, i2] = zero(PointType)
-#                         potential[i3, i1, i2] = pot_value
-#                     end
-#                 end
-#             end
-#         end
-#     end 
-#     nothing
-# end
+function paint!(pointtypes, potential, face::AbstractCurvedSurfacePrimitive{T}, geometry, pot_value, grid::CylindricalGrid) where {T}
+    t_idx_r1, t_idx_r2, t_idx_r3 = get_sub_ind_ranges(face, grid)
+    ticks = (grid.axes[1].ticks, grid.axes[2].ticks, grid.axes[3].ticks)
+    eZ = CartesianVector{T}(0,0,1);
+
+    for i2 in eachindex(ticks[2])
+        for i1 in eachindex(ticks[1])
+            l = ConstructiveSolidGeometry.Line(CartesianPoint(CylindricalPoint{T}(ticks[1][i1], ticks[2][i2], zero(T))), eZ)
+            pts = ConstructiveSolidGeometry.intersection(face, l)
+            for pt in pts
+                if pt in geometry
+                    i3 = searchsortednearest(ticks[3], pt[3])
+                    pointtypes[i1, i2, i3] = zero(PointType)
+                    potential[i1, i2, i3] = pot_value
+                end
+            end
+        end
+    end 
+    # For this we need a the function `intersection_with_φ_axis`...
+    # This will be fun... I skip this for now. 
+
+    # for i3 in t_idx_r3 # z
+    #     for i1 in t_idx_r1 # r
+    #         l = ConstructiveSolidGeometry.Line(CartesianPoint{T}(ticks[1][i1], zero(T), ticks[3][i3]), eY)
+    #         pt = ConstructiveSolidGeometry.intersection(plane, l)
+    #         if pt in geometry
+    #             i2 = searchsortednearest(ticks[2], pt[2])
+    #             pointtypes[i1, i2, i3] = zero(PointType)
+    #             potential[i1, i2, i3] = pot_value
+    #         end
+    #     end
+    # end 
+    
+    for i3 in t_idx_r3 # z;   Maybe switch loops so that the direction of `l` has to be calculated less times..
+        o = CartesianPoint{T}(zero(T), zero(T), ticks[3][i3])
+        for i2 in eachindex(ticks[2]) # φ
+            dir = CartesianVector(CartesianPoint(CylindricalPoint{T}(one(T), ticks[2][i2], zero(T)))) 
+            l = ConstructiveSolidGeometry.Line(o, dir) # dir should be normalized
+            pts_car = ConstructiveSolidGeometry.intersection(face, l)
+            for pt_car in pts_car
+                pt_cyl = CylindricalPoint(pt_car)
+                # Intersection must be in positive r direction of dir: pt_cyl[2] == ticks[2][i2]
+                # Use `abs(pt_cyl[2] - ticks[2][i2]) < 0.1` to avoid rounding issues
+                # if it differs, it would always differ by π = 3.141... -> 0.1 is fine
+                if abs(pt_cyl[2] - ticks[2][i2]) < 0.1 && pt_car in geometry
+                    i1 = searchsortednearest(ticks[1], pt_cyl[1])
+                    pointtypes[i1, i2, i3] = zero(PointType)
+                    potential[i1, i2, i3] = pot_value
+                end
+            end
+        end
+    end 
+    nothing
+end
