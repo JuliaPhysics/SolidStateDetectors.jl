fb118c63c62b38de11d23688927853efd5380bde
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-06-23 15:06:01 +0200

    Activate `HexagonalPrism` detector in tests again

--- test/runtests.jl
+++ test/runtests.jl
@@ -61,18 +61,18 @@ end
         @info signalsum
         @test isapprox( signalsum, T(2), atol = 3e-3 )
     end
-#     @testset "Simulate example detector: HexagonalPrism" begin
-#         sim = Simulation{T}(SSD_examples[:Hexagon])
-#         simulate!(sim, max_refinements = 0, verbose = true)
-#         evt = Event([CartesianPoint{T}(0, 5e-4, 1e-3)])
-#         simulate!(evt, sim, Δt = 1e-9, max_nsteps = 10000)
-#         signalsum = T(0)
-#         for i in 1:length(evt.waveforms)
-#             signalsum += abs(evt.waveforms[i].value[end])
-#         end
-#         @info signalsum
-#         @test isapprox( signalsum, T(2), atol = 5e-4 )
-#     end
+    @testset "Simulate example detector: HexagonalPrism" begin
+        sim = Simulation{T}(SSD_examples[:Hexagon])
+        simulate!(sim, max_refinements = 0, verbose = true)
+        evt = Event([CartesianPoint{T}(0, 5e-4, 1e-3)])
+        simulate!(evt, sim, Δt = 1e-9, max_nsteps = 10000)
+        signalsum = T(0)
+        for i in 1:length(evt.waveforms)
+            signalsum += abs(evt.waveforms[i].value[end])
+        end
+        @info signalsum
+        @test isapprox( signalsum, T(2), atol = 5e-4 )
+    end
     @testset "Simulate example detector: CGD" begin
         sim = Simulation{T}(SSD_examples[:CGD])
         simulate!(sim, max_refinements = 2, verbose = true)
