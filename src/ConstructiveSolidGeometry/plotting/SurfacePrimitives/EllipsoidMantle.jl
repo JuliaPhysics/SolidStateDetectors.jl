600441e9c5354a44a80b91bf5c9545d7fb4e6900
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-06-24 10:40:49 +0200

    Add plotting of normals for `EllipsoidMantle`

--- src/ConstructiveSolidGeometry/plotting/SurfacePrimitives/EllipsoidMantle.jl
+++ src/ConstructiveSolidGeometry/plotting/SurfacePrimitives/EllipsoidMantle.jl
@@ -11,19 +11,36 @@
             ls[i]
         end
     end
-    # if !haskey(plotattributes, :show_normal) || plotattributes[:show_normal]
-    #     @series begin
-    #         label := nothing
-    #         seriestype := :vector
-    #         nφ = cm.φ == nothing ? 0 : (cm.φ[2] + cm.φ[1])/2 
-    #         T = typeof(cm.hZ)
-    #         npt_obj = CartesianPoint(CylindricalPoint{T}(radius_at_z(cm, zero(T)), nφ, zero(T)))
-    #         npt = _transform_into_global_coordinate_system(npt_obj, cm)
-    #         npt, normalize(normal(cm, npt))/10
-    #     end
-    # end
+    if (!haskey(plotattributes, :show_normal) || plotattributes[:show_normal]) &&
+            em.φ === nothing && em.θ === nothing
+        @series begin
+            label := nothing
+            seriestype := :vector
+            pts = _plt_points_for_normals(em)
+            ns = broadcast(p -> normal(em, p) / 5, pts)
+            [(pts[i], ns[i]) for i in eachindex(pts)]
+        end
+    end
 end
 
+function _plt_points_for_normals(em::EllipsoidMantle{T,NTuple{3,T}}) where {T}
+    pts = [ CartesianPoint{T}( em.r[1], zero(T), zero(T)),
+            CartesianPoint{T}(-em.r[1], zero(T), zero(T)),
+            CartesianPoint{T}(zero(T),  em.r[2], zero(T)),
+            CartesianPoint{T}(zero(T), -em.r[2], zero(T)),
+            CartesianPoint{T}(zero(T), zero(T),  em.r[3]),
+            CartesianPoint{T}(zero(T), zero(T), -em.r[3]) ]
+    _transform_into_global_coordinate_system(pts, em)
+end
+function _plt_points_for_normals(em::EllipsoidMantle{T,T}) where {T}
+    pts = [ CartesianPoint{T}( em.r, zero(T), zero(T)),
+            CartesianPoint{T}(-em.r, zero(T), zero(T)),
+            CartesianPoint{T}(zero(T),  em.r, zero(T)),
+            CartesianPoint{T}(zero(T), -em.r, zero(T)),
+            CartesianPoint{T}(zero(T), zero(T),  em.r),
+            CartesianPoint{T}(zero(T), zero(T), -em.r) ]
+    _transform_into_global_coordinate_system(pts, em)
+end
 # function get_plot_points(s::SphereMantle{T}; n = 30) where {T <: AbstractFloat}
 #     plot_points = Vector{CartesianPoint{T}}[]
 
