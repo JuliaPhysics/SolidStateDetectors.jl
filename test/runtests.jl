96cc3c30ee550aa4edd0a053491df468c557917d
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-06-25 14:41:31 +0200

    Activate `CoaxialTorus` detector in tests again

--- test/runtests.jl
+++ test/runtests.jl
@@ -98,19 +98,19 @@ end
         @info signalsum
         @test isapprox( signalsum, T(2), atol = 5e-4 )
     end 
-#     @testset "Simulate example detector: Toroidal" begin
-#         sim = Simulation{T}(SSD_examples[:CoaxialTorus])
-#         SolidStateDetectors.apply_initial_state!(sim, ElectricPotential)
-#         simulate!(sim, max_refinements = 1, verbose = true)
-#         evt = Event([CartesianPoint{T}(0.01,0,0.003)])
-#         simulate!(evt, sim, Δt = 1e-9, max_nsteps = 10000)
-#         signalsum = T(0)
-#         for i in 1:length(evt.waveforms)
-#             signalsum += abs(evt.waveforms[i].value[end])
-#         end
-#         @info signalsum
-#         @test isapprox( signalsum, T(2), atol = 5e-4 )
-#     end
+    @testset "Simulate example detector: Toroidal" begin
+        sim = Simulation{T}(SSD_examples[:CoaxialTorus])
+        SolidStateDetectors.apply_initial_state!(sim, ElectricPotential)
+        simulate!(sim, max_refinements = 1, verbose = true)
+        evt = Event([CartesianPoint{T}(0.01,0,0.003)])
+        simulate!(evt, sim, Δt = 1e-9, max_nsteps = 10000)
+        signalsum = T(0)
+        for i in 1:length(evt.waveforms)
+            signalsum += abs(evt.waveforms[i].value[end])
+        end
+        @info signalsum
+        @test isapprox( signalsum, T(2), atol = 5e-3 )
+    end
 #     =#
 #     @testset "Simulate example detector: SigGen PPC" begin
 #         sim = Simulation{T}(SSD_examples[:SigGen])
