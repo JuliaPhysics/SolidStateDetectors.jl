7aa2d3d69ec4fcd5096223ae172d251c883aa677
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-06-25 14:54:55 +0200

    Activate `SigGen` detector in tests again

--- test/runtests.jl
+++ test/runtests.jl
@@ -111,19 +111,18 @@ end
         @info signalsum
         @test isapprox( signalsum, T(2), atol = 5e-3 )
     end
-#     =#
-#     @testset "Simulate example detector: SigGen PPC" begin
-#         sim = Simulation{T}(SSD_examples[:SigGen])
-#         simulate!(sim, max_refinements = 0, verbose = true)
-#         evt = Event(CartesianPoint.([CylindricalPoint{T}(20e-3, deg2rad(10), 40e-3 )]))
-#         simulate!(evt, sim, Δt = 1e-9, max_nsteps = 10000)
-#         signalsum = T(0)
-#         for i in 1:length(evt.waveforms)
-#             signalsum += abs(evt.waveforms[i].value[end])
-#         end
-#         @info signalsum
-#         @test isapprox( signalsum, T(2), atol = 5e-4 )
-#     end
+    @testset "Simulate example detector: SigGen PPC" begin
+        sim = Simulation{T}(SSD_examples[:SigGen])
+        simulate!(sim, max_refinements = 0, verbose = true)
+        evt = Event(CartesianPoint.([CylindricalPoint{T}(20e-3, deg2rad(10), 40e-3 )]))
+        simulate!(evt, sim, Δt = 1e-9, max_nsteps = 10000)
+        signalsum = T(0)
+        for i in 1:length(evt.waveforms)
+            signalsum += abs(evt.waveforms[i].value[end])
+        end
+        @info signalsum
+        @test isapprox( signalsum, T(2), atol = 2e-3 )
+    end
 end
 
 # @testset "ADLChargeDriftModel" begin
