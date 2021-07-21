b7faa705f658022ec3098665b285fc1a3c5cb310
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-07-21 14:58:36 +0200

    Extend docstrings

--- src/Simulation/Simulation.jl
+++ src/Simulation/Simulation.jl
@@ -737,17 +737,19 @@ There are several `<keyword arguments>` which can be used to tune the computatio
 - `convergence_limit::Real`: `convergence_limit` times the bias voltage sets the convergence limit of the relaxation.
     The convergence value is the absolute maximum difference of the potential between two iterations of all grid points.
     Default of `convergence_limit` is `2e-6` (times bias voltage).
-- refinement_limits `rl`: Defines the maximum relative (to applied bias voltage) allowed differences 
+- `refinement_limits`: Defines the maximum relative (to applied bias voltage) allowed differences 
     of the potential value of neighbored grid points 
     in each dimension for each refinement.
-    - rl::Real -> One refinement with `rl` equal in all 3 dimensions
-    - rl::Tuple{<:Real,<:Real,<:Real} -> One refinement with `rl` set individual for each dimension
-    - rl::Vector{<:Real} -> `length(l)` refinements with `rl[i]` being the limit for the i-th refinement. 
-    - rl::Vector{<:Real,<:Real,<:Real}} -> `length(rl)` refinements with `rl[i]` being the limits for the i-th refinement.
+    - `rl::Real` -> One refinement with `rl` equal in all 3 dimensions
+    - `rl::Tuple{<:Real,<:Real,<:Real}` -> One refinement with `rl` set individual for each dimension
+    - `rl::Vector{<:Real}` -> `length(l)` refinements with `rl[i]` being the limit for the i-th refinement. 
+    - `rl::Vector{<:Real,<:Real,<:Real}}` -> `length(rl)` refinements with `rl[i]` being the limits for the i-th refinement.
 - `min_tick_distance::Tuple{<:Quantity, <:Quantity, <:Quantity}`: Tuple of the minimum allowed distance between 
     two grid ticks for each dimension. It prevents the refinement to make the grid to fine.
+    Default is 1e-5 for linear axes and `0.25 * r_max` for the polar axis in case of a cylindrical grid.
 - `max_tick_distance::Tuple{<:Quantity, <:Quantity, <:Quantity}`: Tuple of the maximum allowed distance between 
     two grid ticks for each dimension used in the initialization of the grid.
+    Default is 1/4 of size of the world of the respective dimension.
 - `grid::Grid`: Initial grid used to start the simulation. Default is `Grid(sim)`.
 - `max_distance_ratio::Real`: Maximum allowed ratio between the two distances in any dimension to the two neighbouring grid points. 
         If the ratio is too large, additional ticks are generated such that the new ratios are smaller than `max_distance_ratio`.
@@ -783,17 +785,19 @@ There are several `<keyword arguments>` which can be used to tune the computatio
 - `convergence_limit::Real`: `convergence_limit` times the bias voltage sets the convergence limit of the relaxation.
     The convergence value is the absolute maximum difference of the potential between two iterations of all grid points.
     Default of `convergence_limit` is `2e-6` (times bias voltage).
-- `refinement_limits: Defines the maximum relative (to applied bias voltage) allowed differences 
+- `refinement_limits`: Defines the maximum relative (to applied bias voltage) allowed differences 
     of the potential value of neighbored grid points 
     in each dimension for each refinement.
-    - rl::Real -> One refinement with `rl` equal in all 3 dimensions
-    - rl::Tuple{<:Real,<:Real,<:Real} -> One refinement with `rl` set individual for each dimension
-    - rl::Vector{<:Real} -> `length(l)` refinements with `rl[i]` being the limit for the i-th refinement. 
-    - rl::Vector{<:Real,<:Real,<:Real}} -> `length(rl)` refinements with `rl[i]` being the limits for the i-th refinement.
+    - `rl::Real` -> One refinement with `rl` equal in all 3 dimensions
+    - `rl::Tuple{<:Real,<:Real,<:Real}` -> One refinement with `rl` set individual for each dimension
+    - `rl::Vector{<:Real}` -> `length(l)` refinements with `rl[i]` being the limit for the i-th refinement. 
+    - `rl::Vector{<:Real,<:Real,<:Real}}` -> `length(rl)` refinements with `rl[i]` being the limits for the i-th refinement.
 - `min_tick_distance::Tuple{<:Quantity, <:Quantity, <:Quantity}`: Tuple of the minimum allowed distance between 
     two grid ticks for each dimension. It prevents the refinement to make the grid to fine.
+    Default is 1e-5 for linear axes and `0.25 * r_max` for the polar axis in case of a cylindrical grid.
 - `max_tick_distance::Tuple{<:Quantity, <:Quantity, <:Quantity}`: Tuple of the maximum allowed distance between 
     two grid ticks for each dimension used in the initialization of the grid.
+    Default is 1/4 of size of the world of the respective dimension.
 - `grid::Grid`: Initial grid used to start the simulation. Default is `Grid(sim)`.
 - `max_distance_ratio::Real`: Maximum allowed ratio between the two distances in any dimension to the two neighbouring grid points. 
         If the ratio is too large, additional ticks are generated such that the new ratios are smaller than `max_distance_ratio`.
@@ -875,14 +879,50 @@ function get_signal(sim::Simulation{T, CS}, drift_paths::Vector{EHDriftPath{T}},
 end
 
 """
-    function simulate!(sim::Simulation{T};  refinement_limits = [0.2, 0.1, 0.05],
-                            max_tick_distance::Union{Missing, length_unit, Tuple{length_unit, angle_unit, length_unit}} = missing,
-                            max_distance_ratio::Real = 5,
-                            verbose::Bool = false,
-                            depletion_handling::Bool = false, 
-                            convergence_limit::Real = 1e-5 ) where {T <: SSDFloat}
+    function simulate!( sim::Simulation{T};  
+                    refinement_limits = [0.2, 0.1, 0.05],
+                    min_tick_distance::Union{Missing, length_unit, Tuple{length_unit, angle_unit, length_unit}} = missing,
+                    max_tick_distance::Union{Missing, length_unit, Tuple{length_unit, angle_unit, length_unit}} = missing,
+                    max_distance_ratio::Real = 5,
+                    verbose::Bool = false,
+                    use_nthreads::Int = Base.Threads.nthreads(),
+                    sor_consts::Union{Missing, <:Real, Tuple{<:Real,<:Real}} = missing,
+                    max_n_iterations::Int = -1,
+                    depletion_handling::Bool = false, 
+                    convergence_limit::Real = 1e-7 ) where {T <: SSDFloat}
 
-ToDo...
+# Keywords
+- `convergence_limit::Real`: `convergence_limit` times the bias voltage sets the convergence limit of the relaxation.
+    The convergence value is the absolute maximum difference of the potential between two iterations of all grid points.
+    Default of `convergence_limit` is `2e-6` (times bias voltage).
+- `refinement_limits`: Defines the maximum relative (to applied bias voltage) allowed differences 
+    of the potential value of neighbored grid points 
+    in each dimension for each refinement.
+    - `rl::Real` -> One refinement with `rl` equal in all 3 dimensions
+    - `rl::Tuple{<:Real,<:Real,<:Real}` -> One refinement with `rl` set individual for each dimension
+    - `rl::Vector{<:Real}` -> `length(l)` refinements with `rl[i]` being the limit for the i-th refinement. 
+    - `rl::Vector{<:Real,<:Real,<:Real}}` -> `length(rl)` refinements with `rl[i]` being the limits for the i-th refinement.
+- `min_tick_distance::Tuple{<:Quantity, <:Quantity, <:Quantity}`: Tuple of the minimum allowed distance between 
+    two grid ticks for each dimension. It prevents the refinement to make the grid to fine.
+    Default is 1e-5 for linear axes and `0.25 * r_max` for the polar axis in case of a cylindrical grid.
+- `max_tick_distance::Tuple{<:Quantity, <:Quantity, <:Quantity}`: Tuple of the maximum allowed distance between 
+    two grid ticks for each dimension used in the initialization of the grid.
+    Default is 1/4 of size of the world of the respective dimension.
+- `max_distance_ratio::Real`: Maximum allowed ratio between the two distances in any dimension to the two neighbouring grid points. 
+        If the ratio is too large, additional ticks are generated such that the new ratios are smaller than `max_distance_ratio`.
+        Default is `5`.
+- `depletion_handling::Bool`: Enables the handling of undepleted regions. Default is false.
+- `use_nthreads::Int`: Number of threads to use in the computation. Default is `Base.Threads.nthreads()`.
+    The environment variable `JULIA_NUM_THREADS` must be set appropriately before the Julia session was
+    started (e.g. `export JULIA_NUM_THREADS=8` in case of bash).
+- `sor_consts::Union{<:Real, NTuple{2, <:Real}}`: Two element tuple in case of cylindrical coordinates.
+    First element contains the SOR constant for `r` = 0.
+    Second contains the constant at the outer most grid point in `r`. A linear scaling is applied in between.
+    First element should be smaller than the second one and both should be ∈ [1.0, 2.0]. Default is [1.4, 1.85].
+    In case of cartesian coordinates only one value is taken.
+- `max_n_iterations::Int`: Set the maximum number of iterations which are performed after each grid refinement.
+    Default is `-1`. If set to `-1` there will be no limit.
+- `verbose::Bool=true`: Boolean whether info output is produced or not.
 """
 function simulate!( sim::Simulation{T};  
                     refinement_limits = [0.2, 0.1, 0.05],
@@ -890,6 +930,9 @@ function simulate!( sim::Simulation{T};
                     max_tick_distance::Union{Missing, length_unit, Tuple{length_unit, angle_unit, length_unit}} = missing,
                     max_distance_ratio::Real = 5,
                     verbose::Bool = false,
+                    use_nthreads::Int = Base.Threads.nthreads(),
+                    sor_consts::Union{Missing, <:Real, Tuple{<:Real,<:Real}} = missing,
+                    max_n_iterations::Int = -1,
                     depletion_handling::Bool = false, 
                     convergence_limit::Real = 1e-7 ) where {T <: SSDFloat}
     calculate_electric_potential!(  sim,
@@ -897,6 +940,9 @@ function simulate!( sim::Simulation{T};
                                     min_tick_distance = min_tick_distance,
                                     max_tick_distance = max_tick_distance,
                                     max_distance_ratio = max_distance_ratio,
+                                    use_nthreads = use_nthreads,
+                                    sor_consts = sor_consts,
+                                    max_n_iterations = max_n_iterations,
                                     verbose = verbose,
                                     depletion_handling = depletion_handling,
                                     convergence_limit = convergence_limit )
