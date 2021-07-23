9238831bef7dc3075d7265ad947ae44c2e37a864
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-07-23 11:46:23 +0200

    Add docstring for `SSD_examples`

--- src/examples.jl
+++ src/examples.jl
@@ -8,43 +8,23 @@ function get_path_to_example_config_files()::String
     return joinpath(@__DIR__, "../examples/example_config_files")
 end
 
-const SSD_examples = Dict{Symbol,String}()
-SSD_examples[:Coax] = (
-    joinpath(@__DIR__, "../examples/example_config_files/public_Coax_config.yaml")
-)
-SSD_examples[:InvertedCoax] = (
-    joinpath(@__DIR__, "../examples/example_config_files/public_ivc_config.yaml")
-)
-SSD_examples[:InvertedCoaxInCryostat] = (
-    joinpath(@__DIR__, "../examples/example_config_files/ivc_splitted_config/public_ivc_cryostat_config.yaml")
-)
-SSD_examples[:BEGe] = (
-    joinpath(@__DIR__, "../examples/example_config_files/public_SegBEGe_config.yaml")
-)
-SSD_examples[:CGD] = (
-    joinpath(@__DIR__, "../examples/example_config_files/public_CGD_config.yaml")
-)
-SSD_examples[:CGD_CylGrid] = (
-    joinpath(@__DIR__, "../examples/example_config_files/public_CGD_config_cyl_grid.yaml")
-)
-SSD_examples[:Spherical] = (
-    joinpath(@__DIR__, "../examples/example_config_files/public_spherical_detector_config.yaml")
-)
-SSD_examples[:SigGen] = (
-    joinpath(@__DIR__, "../examples/example_config_files/public_ppc_config_SigGen.config")
-)
-SSD_examples[:InfiniteParallelPlateCapacitor] = (
-    joinpath(@__DIR__, "../examples/example_config_files/infinite_parallel_plate_capacitor.yaml")
-)
-SSD_examples[:InfiniteCoaxialCapacitor] = (
-    joinpath(@__DIR__, "../examples/example_config_files/infinite_coaxial_capacitor.yaml")
-)
-SSD_examples[:InfiniteCoaxialCapacitorCartesianCoords] = (
-    joinpath(@__DIR__, "../examples/example_config_files/infinite_coaxial_capacitor_cartesian_coords.yaml")
-)
-SSD_examples[:Hexagon] = (
-    joinpath(@__DIR__, "../examples/example_config_files/minimum_hexagon_config.yaml")
-)
-SSD_examples[:CoaxialTorus] = (
-    joinpath(@__DIR__, "../examples/example_config_files/public_coaxial_torus_config.yaml")
-)
+"""
+    SSD_examples::Dict{Symbol,String}
+
+containing absolute paths to example configuration files.
+"""
+const SSD_examples = Dict{Symbol,String}([
+    :InvertedCoax   => joinpath(@__DIR__, "../examples/example_config_files/public_ivc_config.yaml"),
+    :InvertedCoaxInCryostat => joinpath(@__DIR__, "../examples/example_config_files/ivc_splitted_config/public_ivc_cryostat_config.yaml"),
+    :Coax           => joinpath(@__DIR__, "../examples/example_config_files/public_Coax_config.yaml"),
+    :BEGe => joinpath(@__DIR__, "../examples/example_config_files/public_SegBEGe_config.yaml"),
+    :CGD => joinpath(@__DIR__, "../examples/example_config_files/public_CGD_config.yaml"),
+    :CGD_CylGrid => joinpath(@__DIR__, "../examples/example_config_files/public_CGD_config_cyl_grid.yaml"),
+    :Spherical => joinpath(@__DIR__, "../examples/example_config_files/public_spherical_detector_config.yaml"),
+    :SigGen => joinpath(@__DIR__, "../examples/example_config_files/public_ppc_config_SigGen.config"),
+    :InfiniteParallelPlateCapacitor => joinpath(@__DIR__, "../examples/example_config_files/infinite_parallel_plate_capacitor.yaml"),
+    :InfiniteCoaxialCapacitor => joinpath(@__DIR__, "../examples/example_config_files/infinite_coaxial_capacitor.yaml"),
+    :InfiniteCoaxialCapacitorCartesianCoords => joinpath(@__DIR__, "../examples/example_config_files/infinite_coaxial_capacitor_cartesian_coords.yaml"),
+    :Hexagon => joinpath(@__DIR__, "../examples/example_config_files/minimum_hexagon_config.yaml"),
+    :CoaxialTorus => joinpath(@__DIR__, "../examples/example_config_files/public_coaxial_torus_config.yaml"),
+])
