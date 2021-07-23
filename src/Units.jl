984e8ad979c0649c4846890845f9df7996c2248c
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-07-23 16:13:41 +0200

    Add checks to units parsing

--- src/Units.jl
+++ src/Units.jl
@@ -32,27 +32,51 @@ unit_conversion = Dict{String, Unitful.Units}(
     "K" => u"K", "Kelvin" => u"K", "C" => u"°C", "Celsius" => u"°C", #temperature
 )
 
-function default_unit_tuple()::NamedTuple{<:Any, <:NTuple{4, Unitful.Units}}
+const UnitTuple = NamedTuple{(:length, :angle, :potential, :temperature), Tuple{
+                    Unitful.FreeUnits{<:Any, Unitful.𝐋}, 
+                    Unitful.FreeUnits{<:Any, NoDims}, 
+                    Unitful.FreeUnits{<:Any, Unitful.𝐋^2 * Unitful.𝐌 * Unitful.𝐈^-1 * Unitful.𝐓^-3}, 
+                    Unitful.FreeUnits{<:Any, Unitful.𝚯}}}
+
+function default_unit_tuple()::UnitTuple
     return (
-        length = u"m", # change this to u"m" ? SI Units
-        potential = u"V",
+        length = u"m",
         angle = u"°",
+        potential = u"V",
         temperature = u"K"
     )
 end
 
-function construct_units(config_file_dict::AbstractDict)
+function construct_unit(ustring::String)::Unitful.Units
+    try
+        parsed_unit = uparse(ustring)
+        @assert parsed_unit isa Unitful.Units
+        return parsed_unit
+    catch 
+        @assert ustring in keys(unit_conversion) "Unit string $(ustring) cannot be interpreted..."
+        return unit_conversion[ustring]
+    end
+end
+
+function construct_units(config_file_dict::AbstractDict)::UnitTuple
     dunits::NamedTuple = default_unit_tuple()
     if haskey(config_file_dict, "units")
         d = config_file_dict["units"]
         dunits = (
-            length = haskey(d, "length") ? unit_conversion[d["length"]] : dunits.length, 
-            angle  = haskey(d, "angle") ? unit_conversion[d["angle"]] : dunits.angle,
-            potential = haskey(d, "potential") ? unit_conversion[d["potential"]] : dunits.potential,
-            temperature = haskey(d, "temperature") ? unit_conversion[d["temperature"]] : dunits.temperature
+            length = haskey(d, "length") ? construct_unit(d["length"]) : dunits.length, 
+            angle  = haskey(d, "angle") ? construct_unit(d["angle"]) : dunits.angle,
+            potential = haskey(d, "potential") ? construct_unit(d["potential"]) : dunits.potential,
+            temperature = haskey(d, "temperature") ? construct_unit(d["temperature"]) : dunits.temperature
         )
     end
-    dunits
+    matching_units::Bool = dimension.(values(dunits)) == (Unitful.𝐋, Unitful.NoDims, Unitful.𝐋^2 * Unitful.𝐌 * Unitful.𝐈^-1 * Unitful.𝐓^-3, Unitful.𝚯) 
+    @assert matching_units "Some of the units are not given in the right format:"*
+        (dimension(dunits.length)      == Unitful.𝐋       ? "" : "\n  - units.length ($(dunits.length)) does not describe a length.")*
+        (dimension(dunits.angle)       == Unitful.NoDims  ? "" : "\n  - units.angle ($(dunits.angle)) does not describe an angle.")*
+        (dimension(dunits.potential)   == Unitful.𝐋^2 * Unitful.𝐌 * Unitful.𝐈^-1 * Unitful.𝐓^-3 ? "" : "\n  - units.potential ($(dunits.potential)) does not describe a potential.")*
+        (dimension(dunits.temperature) == Unitful.𝚯       ? "" : "\n  - units.temperature ($(dunits.temperature)) does not describe a temperature.")
+        
+    return UnitTuple(dunits)
 end
 
 
