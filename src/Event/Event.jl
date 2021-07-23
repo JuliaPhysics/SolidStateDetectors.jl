e0d095dd5adbf30e4218d48ba39c358af1f9f7e0
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-07-23 13:34:28 +0200

    Add docstring for `Event` and `simulate!(evt::Event, sim::Simulation)`

--- src/Event/Event.jl
+++ src/Event/Event.jl
@@ -1,8 +1,15 @@
 """
     mutable struct Event{T <: SSDFloat}
 
-Collection struct for individual events. This (mutable) struct is ment to be used to look at individual events,
+Collection struct for individual events. 
+This (mutable) struct is mend to be used to look at individual events,
 not to process a huge amount of events.
+
+# Fields:
+- `locations::Union{Vector{<:AbstractCoordinatePoint{T}}, Missing}`: Vector of the positions of all hits of the event.
+- `energies::Union{Vector{T}, Missing}`: Vector of energies corresponding to the hits of the event.
+- `drift_paths::Union{Vector{EHDriftPath{T}}, Missing}`: Calculated drift paths of each hit position. 
+- `waveforms::Union{Vector{<:Any}, Missing}`: Generated signals (waveforms) of the event.
 """
 mutable struct Event{T <: SSDFloat}
     locations::Union{Vector{<:AbstractCoordinatePoint{T}}, Missing}
@@ -69,6 +76,19 @@ function get_signals!(event::Event{T}, sim::Simulation{T}; Δt::RealQuantity = 5
     nothing
 end
 
+"""
+    function simulate!(event::Event{T}, sim::Simulation{T}; max_nsteps::Int = 1000, Δt::RealQuantity = 5u"ns", verbose::Bool = true)::Nothing where {T <: SSDFloat}
+
+Simulates the `event` for a given Simulation `sim`:
+    1. Simulate the drift paths of all hits, at `event.locations`, and stores them under `event.drift_paths`. 
+    2. Generate the signal (waveform) for all channels for which a weighting potential is specified in `sim.weighting_potentials`
+        and stores them under `event.waveforms`.
+
+# Keywords
+- `Δt::RealQuantity = 5u"ns"`: Time difference between two time stamps of the drift and the signals.
+- `max_nsteps::Int = 1000`: Maximum number of steps in the drift of each hit. 
+- `verbose = false`: Activate or deactivate additional info output. 
+"""
 function simulate!(event::Event{T}, sim::Simulation{T}; max_nsteps::Int = 1000, Δt::RealQuantity = 5u"ns", verbose::Bool = true)::Nothing where {T <: SSDFloat}
     drift_charges!(event, sim, max_nsteps = max_nsteps, Δt = Δt, verbose = verbose)
     get_signals!(event, sim, Δt = Δt)
