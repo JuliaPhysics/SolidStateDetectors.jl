bc57f629f9b6e709a2f1e83e5bd7a65a69d8e349
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-07-27 23:30:33 +0200

    Update docstrings for `ElectricPotential`

--- src/PotentialSimulation/ElectricPotential.jl
+++ src/PotentialSimulation/ElectricPotential.jl
@@ -1,18 +1,17 @@
 """
     struct ElectricPotential{T, N, S, AT} <: AbstractArray{T, N}
-- `T`: Element type of `data`.
-- `N`: Dimension of the `grid` and `data` array.  
-- `S`: Coordinate system (`Cartesian` or `Cylindrical`).
-- `AT`: Axes type.  
         
-# Fields
-- `data::Array{T, N}`
-- `grid::Grid{T, N, S, AT}`
-
-The `data` array contains the values of the electric potential at the discrete points defined by the 
-axes ticks of the `grid`. 
-
-The unit of the values in `data` is volts ([V]).
+Electric potential of the simulation in units of volt (V).
+        
+## Parametric types 
+* `T`: Element type of `data`.
+* `N`: Dimension of the `grid` and `data` array.  
+* `S`: Coordinate system (`Cartesian` or `Cylindrical`).
+* `AT`: Axes type.  
+        
+## Fields
+* `data::Array{T, N}`: Array containing the values of the electric potential at the discrete points of the `grid`.
+* `grid::Grid{T, N, S, AT}`: Grid defining the discrete points for which the electric potential is determined.
 """
 struct ElectricPotential{T, N, S, AT} <: AbstractArray{T, N}
     data::Array{T, N}
