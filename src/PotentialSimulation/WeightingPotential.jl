7459bc66a8864b9577a1e3a5bef007ffea3394a1
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-07-27 23:32:17 +0200

    Update docstrings for `WeightingPotential`

--- src/PotentialSimulation/WeightingPotential.jl
+++ src/PotentialSimulation/WeightingPotential.jl
@@ -1,18 +1,17 @@
 """
     struct WeightingPotential{T, N, S, AT} <: AbstractArray{T, N}
-- `T`: Element type of `data`.
-- `N`: Dimension of the `grid` and `data` array.  
-- `S`: Coordinate system (`Cartesian` or `Cylindrical`).
-- `AT`: Axes type.  
         
-# Fields
-- `data::Array{T, N}`
-- `grid::Grid{T, N, S, AT}`
-
-The `data` array contains the values of the weighting potential at the discrete points defined by the 
-axes ticks of the `grid`. 
-
-The unit of the values in `data` is the unit one ([1]).
+Weighting potential for a given contact which is a unitless quantity.
+        
+## Parametric types 
+* `T`: Element type of `data`.
+* `N`: Dimension of the `grid` and `data` array.  
+* `S`: Coordinate system (`Cartesian` or `Cylindrical`).
+* `AT`: Axes type.  
+        
+## Fields
+* `data::Array{T, N}`: Array containing the values of the weighting potential at the discrete points of the `grid`.
+* `grid::Grid{T, N, S, AT}`: Grid defining the discrete points for which the weighting potential is determined.
 """
 struct WeightingPotential{T, N, S <: AbstractCoordinateSystem, AT} <: AbstractArray{T, N}
     data::Array{T, N}
