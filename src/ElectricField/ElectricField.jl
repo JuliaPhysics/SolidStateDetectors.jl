455ebe603a8cec8b455187c419b97f781c349c8f
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-07-28 00:34:47 +0200

    Add docstrings for `ElectricField`

--- src/ElectricField/ElectricField.jl
+++ src/ElectricField/ElectricField.jl
@@ -1,3 +1,18 @@
+"""
+    struct ElectricField{T, N, S, AT} <: AbstractArray{T, N}
+        
+Electric field of the simulation in units of volt per meter (V/m).
+        
+## Parametric types 
+* `T`: Element type of `grid.axes`.
+* `N`: Dimension of the `grid` and `data` array.  
+* `S`: Coordinate system (`Cartesian` or `Cylindrical`).
+* `AT`: Axes type.
+        
+## Fields
+* `data::Array{<:StaticArray{Tuple{N}, T}, N}`: Array containing the field vectors of the electric field at the discrete points of the `grid`.
+* `grid::Grid{T, N, S, AT}`: Grid defining the discrete points for which the electric field is determined.
+"""
 struct ElectricField{T, N, S, AT} <: AbstractArray{T, N}
     data::Array{<:StaticArray{Tuple{N}, T}, N}
     grid::Grid{T, N, S, AT}
