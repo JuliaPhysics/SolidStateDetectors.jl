d7a1c24c5d88549d90d256b00aed0cc6c6b3dcfa
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-07-27 19:22:15 +0200

    Update docstring for `ElectricFieldChargeDriftModel`

--- src/ChargeDriftModels/ElectricFieldChargeDriftModel/ElectricFieldChargeDriftModel.jl
+++ src/ChargeDriftModels/ElectricFieldChargeDriftModel/ElectricFieldChargeDriftModel.jl
@@ -1,5 +1,9 @@
 """
-    ElectricFieldChargeDriftModel <: AbstractChargeDriftModel
+    struct ElectricFieldChargeDriftModel{T <: SSDFloat} <: AbstractChargeDriftModel{T}
+        
+Charge drift model in which the electrons and holes drift along the electric field with a mobility of ± 1m²/Vs.
+
+This model is the default when no charge drift model is defined in the configuration file.
 """
 struct ElectricFieldChargeDriftModel{T <: SSDFloat} <: AbstractChargeDriftModel{T} end
 
