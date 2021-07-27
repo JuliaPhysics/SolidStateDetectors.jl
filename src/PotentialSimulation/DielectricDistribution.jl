7f4b397988782f5aca8a0ff50ec7322d262a4487
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-07-27 23:36:30 +0200

    Update docstrings for `DielectricDistribution`

--- src/PotentialSimulation/DielectricDistribution.jl
+++ src/PotentialSimulation/DielectricDistribution.jl
@@ -1,19 +1,24 @@
 """
     struct DielectricDistribution{T, N, S, AT} <: AbstractArray{T, N}
-- `T`: Element type of `data`.
-- `N`: Dimension of the `grid` and `data` array.  
-- `S`: Coordinate system (`Cartesian` or `Cylindrical`).
-- `AT`: Axes type.  
         
-# Fields
-- `data::Array{T, N}`
-- `grid::Grid{T, N, S, AT}`
+Dielectric distribution, or distribution of the relative permittivity, ``\\epsilon_r``, 
+needed to calculate the [`ElectricPotential`](@ref).
+The units of the dielectric distribution is unitless.
+        
+## Parametric types 
+* `T`: Element type of `data`.
+* `N`: Dimension of the `grid` and `data` array.  
+* `S`: Coordinate system (`Cartesian` or `Cylindrical`).
+* `AT`: Axes type.  
+        
+## Fields
+* `data::Array{T, N}`: Array containing the values of the dielectric distribution at the discrete points of the `grid`.
+* `grid::Grid{T, N, S, AT}`: Grid defining the discrete points at which the electric potential is determined.
 
-The `data` array contains the values of the dielectric distribution at the discrete points 
-between the points defined by the axes ticks of the extended grid of `grid`.\n
-Thus, `size(data) == size(grid) .+ 1` !
-
-The unit of the values in `data` is the unit one ([1]).
+!!! note
+    The `data` array contains the values of the dielectric distribution at the discrete points 
+    between the points defined by the axes ticks of the extended grid of `grid`.\n
+    Thus, `size(data) == size(grid) .+ 1` !
 """
 struct DielectricDistribution{T, N, S, AT} <: AbstractArray{T, N}
     data::Array{T, N}
