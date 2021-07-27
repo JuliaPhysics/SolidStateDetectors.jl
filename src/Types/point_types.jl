fecd020aad5d584d55e3d12c651b2a3408b41848
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-07-27 23:40:10 +0200

    Update docstrings for `PointTypes`

--- src/Types/point_types.jl
+++ src/Types/point_types.jl
@@ -24,20 +24,21 @@ const pn_junction_bit = 0x04 # parse(UInt8, "00001000", base=2) # 0 -> point is 
 
 """
     struct PointTypes{T, N, S, AT} <: AbstractArray{T, N}
-- `T`: Element type of `grid.axes`.
-- `N`: Dimension of the `grid` and `data` array.  
-- `S`: Coordinate system (`Cartesian` or `Cylindrical`).
-- `AT`: Axes type.  
         
-# Fields
-- `data::Array{PointType, N}`
-- `grid::Grid{T, N, S, AT}`
+Information about the grid points used to calculate the [`ElectricPotential`](@ref)
+stored via bit-flags. Data is stored as [`PointType`](@ref) which is an `UInt8`.
+        
+## Parametric types 
+* `T`: Element type of `grid.axes`.
+* `N`: Dimension of the `grid` and `data` array.  
+* `S`: Coordinate system (`Cartesian` or `Cylindrical`).
+* `AT`: Axes type.  
+        
+## Fields
+* `data::Array{PointType, N}`: Array containing the point type values at the discrete points of the `grid`.
+* `grid::Grid{T, N, S, AT}`: Grid defining the discrete points for which the point types are determined.
 
-The `data` array contains the point types of the discrete points defined by the 
-axes ticks of the `grid`. 
-
-The element type `data` is `PointType` which is an `UInt8` and stores is information bitwise,
-see [`PointType`](@ref).
+See also [`PointType`](@ref).
 """
 struct PointTypes{T, N, S, AT} <: AbstractArray{T, N}
     data::Array{PointType, N}
