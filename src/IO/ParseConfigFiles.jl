a72615437204d3948a11c8af0aa45a3568e02aa1
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-07-28 23:00:05 +0200

    Add information on how to update config files

--- src/IO/ParseConfigFiles.jl
+++ src/IO/ParseConfigFiles.jl
@@ -1,8 +1,20 @@
 """
-    parse_config_file(filename::AbstractString)::SolidStateDetector{T} where {T <: SSDFloat}
+    parse_config_file(filename::AbstractString)::Dict where {T <: SSDFloat}
 
-Reads in a config file and returns an Detector struct which holds all information specified in the config file.
-Currently supported formats for the config file: .json, .yaml
+Reads in a configuration file and returns a parsed dictionary which holds all the
+information specified in the configuration file.
+
+Find detailed information on configuration files [here](https://juliaphysics.github.io/SolidStateDetectors.jl/stable/man/config_files/#Config-Files).
+
+## Arguments 
+* `filename::AbstractString`: File name of the configuration file. If the file is not
+    in the same directory, a path to the file is required.
+
+!!! note
+    Currently supported formats for the configuration files:
+        - [YAML](https://github.com/JuliaData/YAML.jl): `filename` ends with `.yaml`.
+        - [JSON](https://github.com/JuliaIO/JSON.jl): `filename` ends with `.json`.
+        - [SigGen](https://github.com/radforddc/icpc_siggen): `filename` ends with `.config`.
 """
 function parse_config_file(filename::AbstractString)::Dict{Any,Any}
     if endswith(filename, ".toml")
@@ -20,6 +32,18 @@ function parse_config_file(filename::AbstractString)::Dict{Any,Any}
     else
         error("Currently only .json and .yaml files are supported.")
     end
+    @assert !haskey(dict, "objects") "Configuration file deprecation.\n
+        The configuration file format was updated in v0.6.0.
+        However, this configuration file still seems to be in the old format.\n
+        To update your configuration file to the new format (v0.6.0 and newer),
+        open a new Julia session and load the following file:\n
+        \tinclude(\"<path_to_SolidStateDetectors.jl>/test/update_config_files.jl\")\n
+        Afterwards, run\n
+        \tupdate_config_file(\"<path_to_configuration_file>\")\n
+        This method returns the file name of the updated configuration file.
+        Please close the Julia session after updating the configuration files, as some
+        parsing methods are overridden with old methods."
+        
     dict
 end
 
