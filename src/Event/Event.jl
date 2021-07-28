fd833296dad3ed183a4e10c6cf19e8b11230d056
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-07-28 20:58:16 +0200

    Update docstrings for `drift_charges!`

--- src/Event/Event.jl
+++ src/Event/Event.jl
@@ -51,13 +51,25 @@ in(evt::Event, sim::Simulation) = all( pt -> pt in sim.detector, evt.locations)
 """
     drift_charges!(evt::Event{T}, sim::Simulation{T}; max_nsteps::Int = 1000, Δt::RealQuantity = 5u"ns", verbose::Bool = true)::Nothing where {T <: SSDFloat}
 
-Calculate the drift paths for the given `evt` and Simulation `sim`
-    and stores them under `evt.drift_paths`. 
+Calculates the drift paths for the given [`Event`](@ref) `evt` and [`Simulation`](@ref) `sim`
+    and stores them in `evt.drift_paths`.
+    
+## Arguments 
+* `evt::Event{T}`: [`Event`](@ref) for which the charges should be drifted.
+* `sim::Simulation{T}`: [`Simulation`](@ref) which defines the setup in which the charges in `evt` should drift.
 
-# Keywords
-- `Δt::RealQuantity = 5u"ns"`: Time difference between two time stamps of the drift.
-- `max_nsteps::Int = 1000`: Maximum number of steps in the drift of each hit. 
-- `verbose = false`: Activate or deactivate additional info output. 
+## Keywords
+* `max_nsteps::Int = 1000`: Maximum number of steps in the drift of each hit. 
+* `Δt::RealQuantity = 5u"ns"`: Time step used for the drift.
+* `verbose = true`: Activate or deactivate additional info output.
+
+## Example 
+```julia 
+drift_charges!(evt, sim, Δt = 1u"ns", verbose = false)
+```
+
+!!! note
+    Using values with units for `Δt` requires the package [Unitful.jl](https://github.com/PainterQubits/Unitful.jl).
 """
 function drift_charges!(evt::Event{T}, sim::Simulation{T}; max_nsteps::Int = 1000, Δt::RealQuantity = 5u"ns", verbose::Bool = true)::Nothing where {T <: SSDFloat}
     evt.drift_paths = drift_charges(sim, CartesianPoint.(evt.locations), Δt = Δt, max_nsteps = max_nsteps, verbose = verbose)
