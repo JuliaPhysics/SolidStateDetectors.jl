abedfa40878e3cc92fd1676ef116789927261f3c
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-07-28 21:03:22 +0200

    Update docstrings for `simulate_waveforms`

--- src/MCEventsProcessing/MCEventsProcessing.jl
+++ src/MCEventsProcessing/MCEventsProcessing.jl
@@ -1,23 +1,49 @@
 include("table_utils.jl")
 
 """
-    simulate_waveforms( mcevents::TypedTables.Table, sim::Simulation{T})
+    simulate_waveforms( mcevents::TypedTables.Table, sim::Simulation{T}; kwargs...)
+    simulate_waveforms( mcevents::TypedTables.Table, sim::Simulation{T}, output_dir::AbstractString, output_base_name::AbstractString; kwargs...)
 
-1. Calculates the drift paths of all energy hits defined in `mcevents`
-    based on the drift fields for electrons and holes stored in `s.electron_drift_field` and 
-    `s.hole_drift_field`.
-2. Determines the signal (waveforms) of all channels 
-    (for which a weighting potential is given in the simulation object `s`.)
-    for the generated drift paths of the hits.
-3. Returns a the input table `mcevents` with an additional column `waveform` 
-    in which the generated waveforms are stored. 
+Simulates the waveforms for all events defined in `mcevents` for a given [`Simulation`] by
 
-Note: The drift paths are just calculated temporarily and not returned. 
+1. calculating the drift paths of all energy hits defined in `mcevents`
+    based on the drift fields for electrons and holes stored in `sim.electron_drift_field` and 
+    `sim.hole_drift_field`, 
+2. determining the signal (waveforms) of all channels, for which a weighting potential 
+    is given in the simulation object `sim`, from the generated drift paths of the hits.
 
-# Keywords
-- `Δt::RealQuantity = 4u"ns"`: Time difference between two time stamps of the drift and the signals.
-- `max_nsteps::Int = 1000`: Maximum number of steps in the drift of each hit. 
-- `verbose = false`: Activate or deactivate additional info output. 
+
+## Arguments
+* `mcevents::TypedTables.Table`: Table with information about events in the simulated setup.
+* `sim::Simulation{T}`: [`Simulation`](@ref) which defines the setup in which the charges in `mcevents` should drift.
+
+If [HDF5.jl](https://github.com/JuliaIO/HDF5.jl) is loaded, this function has additional arguments. 
+
+## Additional Arguments (HDF5)
+* `output_dir::AbstractString`: Directory where the HDF5 output file is saved.
+* `output_base_name::AbstractString`: Basename of the HDF5 output file, default is `"generated_waveforms"`.
+
+## Keywords
+* `max_nsteps::Int = 1000`: Maximum number of steps in the drift of each hit. 
+* `Δt::RealQuantity = 4u"ns"`: Time step used for the drift.
+* `verbose = false`: Activate or deactivate additional info output.
+* `chunk_n_physics_events::Int = 1000` (HDF5 only): Number of events that should be saved in a single HDF5 output file.
+
+## Examples
+```julia 
+simulate_waveforms(mcevents, sim, Δt = 1u"ns", verbose = false)
+# => returns the input table `mcevents` with an additional column `waveform` in which the generated waveforms are stored
+```
+```julia 
+import HDF5
+simulate_waveforms(mcevents, sim, "output_dir", "my_basename", Δt = 1u"ns", verbose = false)
+# => simulates the charge drift and saves the output to "output_dir/my_basename_evts_xx.h5"
+```
+!!! note 
+    The drift paths are just calculated temporarily and not returned.
+
+!!! note
+    Using values with units for `Δt` requires the package [Unitful.jl](https://github.com/PainterQubits/Unitful.jl).
 """
 function simulate_waveforms( mcevents::TypedTables.Table, sim::Simulation{T};
                              Δt::RealQuantity = 4u"ns",
