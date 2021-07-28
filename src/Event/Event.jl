4a14aa33be3b3011daec24ce39244a7b885599dc
Lukas Hauertmann <lhauert@mpp.mpg.de>
2021-07-28 11:42:24 +0200

    Add docstrings for `add_baseline_and_extend_tail`

--- src/Event/Event.jl
+++ src/Event/Event.jl
@@ -105,6 +105,25 @@ function simulate!(event::Event{T}, sim::Simulation{T}; max_nsteps::Int = 1000, 
     nothing
 end
 
+
+"""
+    add_baseline_and_extend_tail(wv::RadiationDetectorSignals.RDWaveform{T,U,TV,UV}, n_baseline_samples::Int, total_waveform_length::Int) where {T,U,TV,UV}
+
+Add a zero-valued baseline in front of the waveform `wv` and extends (or cuts off) the waveform at the end with the last value of `wv`.
+A waveform of length `total_waveform_length` is returned.
+
+## Arguments
+* `wv::RadiationDetectorSignals.RDWaveform{T,U,TV,UV}`: A waveform (signal over time).
+* `n_baseline_samples::Int`: Number of samples added in front of the waveform with values 0. 
+* `total_waveform_length::Int`: Number of samples of the extended waveform which is returned.
+
+## Examples
+    add_baseline_and_extend_tail(wv, 1000, 5000)   
+
+!!! note 
+    This functions assumes that the time steps between the samples of the input waveform `wv` are the same. Thus, that the input waveform is
+    sampled with a fixed frequency. 
+"""
 function add_baseline_and_extend_tail(wv::RadiationDetectorSignals.RDWaveform{T,U,TV,UV}, n_baseline_samples::Int, total_waveform_length::Int) where {T,U,TV,UV}
     new_signal::Vector{eltype(UV)} = Vector{eltype(UV)}(undef, total_waveform_length)
     new_signal[1:n_baseline_samples] .= zero(eltype(UV))
