1b85a4eb935351a2dee23982bc84ca41591c71b0
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-08-20 01:03:28 +0200

    Update conversion `Missing` <-> `NamedTuple`
    The value missing cannot be saved by LegendHDF5IO.jl. It is saved as string now

--- src/IO/IO.jl
+++ src/IO/IO.jl
@@ -1,7 +1,7 @@
 include("SigGenInterface.jl")
 include("ParseConfigFiles.jl")
 
-NamedTuple(::Missing) = (object = missing,)
+NamedTuple(::Missing) = (object = "missing",)
 NamedTuple(d::Dict) = (dict_json_string = json(d),)
 Base.convert(T::Type{NamedTuple}, x::Dict) = T(x)
 Dict(nt::NamedTuple) = JSON.parse(nt.dict_json_string)
