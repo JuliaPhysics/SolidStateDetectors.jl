4c3886234e1ac95c8d7c41fc5de8a1a71119859b
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-09-01 15:39:26 +0200

    Add function to plot the electron and hole contributions to a `RDWaveform`

--- src/Event/Event.jl
+++ src/Event/Event.jl
@@ -230,13 +230,12 @@ simulate!(event, simulation)
 
 contact_id = 1
 wf = get_electron_and_hole_contribution(evt, sim, contact_id)
-plot(add_baseline_and_extend_tail(wf.electron_contribution, 0, 500), label = "Electron contribution", color = :red, lw = 2)
-plot!(add_baseline_and_extend_tail(wf.hole_contribution, 0, 500), label = "Hole contribution", color = :green, lw = 2)
-plot!(add_baseline_and_extend_tail(event.waveforms[contact_id], 0, 500), label = "Sum", color = contact_id, lw = 2, alpha = 0.5)
 ```
 
 !!! note 
     The drift paths in `evt` need to be calculated using [`drift_charges!`](@ref) before calling this function.
+    
+See also [`plot_electron_and_hole_contribution`](@ref).
 """
 function get_electron_and_hole_contribution(evt::Event{T}, sim::Simulation{T, S}, contact_id::Int
             )::NamedTuple{(:electron_contribution, :hole_contribution), <:Tuple{RDWaveform, RDWaveform}} where {T <: SSDFloat, S}
@@ -265,3 +264,78 @@ function get_electron_and_hole_contribution(evt::Event{T}, sim::Simulation{T, S}
 end
 
 export get_electron_and_hole_contribution
+
+
+"""
+    plot_electron_and_hole_contribution(evt::Event{T}, sim::Simulation{T}, contact_id::Int)
+    
+Plots the waveform as well as the electron and hole contribution to the waveform 
+of a [`Contact`](@ref) with a given `contact_id` of an [`Event`](@ref).
+
+## Arguments
+* `evt::Event{T}`: [`Event`](@ref) in which the charges have already been drifted.
+* `sim::Simulation{T}`: [`Simulation`](@ref) which defines the setup in which the charges in `evt` were drifted.
+* `contact_id::Int`: The `id` of the [`Contact`](@ref) for which the waveform should be split into electron and hole contribution.
+
+## Keywords 
+* `n_samples::Int`: Number of samples with which the waveforms will be plotted. The default is the number of samples of the original waveform.
+
+## Example
+```julia 
+using Plots
+using SolidStateDetector
+T = Float32
+
+simulation = Simulation{T}(SSD_examples[:InvertedCoax])
+simulate!(simulation)
+event = Event([CartesianPoint{T}(0.02,0.01,0.05)])
+simulate!(event, simulation)
+
+contact_id = 1
+plot_electron_and_hole_contribution(evt, sim, contact_id, n_samples = 300)
+```
+
+!!! note 
+    The drift paths in `evt` need to be calculated using [`drift_charges!`](@ref) before calling this function.
+    
+!!! note 
+    This method requires to load the package [Plots.jl](https://github.com/JuliaPlots/Plots.jl).
+    
+See also [`get_electron_and_hole_contribution`](@ref).
+"""
+function plot_electron_and_hole_contribution end
+
+@userplot Plot_electron_and_hole_contribution
+@recipe function f(gdd::Plot_electron_and_hole_contribution; linewidth = 2, n_samples = missing)
+    
+    sim::Simulation = gdd.args[2]
+    T = get_precision_type(sim)
+    evt::Event{T} = gdd.args[1]
+    contact_id::Int = gdd.args[3]
+    
+    ismissing(n_samples) ? n_samples = length(evt.waveforms[contact_id].value) : nothing
+    
+    wf::NamedTuple{(:electron_contribution, :hole_contribution), <:Tuple{RDWaveform, RDWaveform}} = get_electron_and_hole_contribution(evt, sim, contact_id)
+    
+    @series begin
+        linecolor --> :red 
+        linewidth --> linewidth
+        label --> "Electron contribution"
+        add_baseline_and_extend_tail(wf.electron_contribution, 0, n_samples)
+    end
+    
+    @series begin
+        linecolor --> :green 
+        linewidth --> linewidth
+        label --> "Hole contribution"
+        add_baseline_and_extend_tail(wf.hole_contribution, 0, n_samples)
+    end
+    
+    @series begin
+        linecolor --> contact_id
+        linewidth --> linewidth
+        seriesalpha --> 0.5
+        label --> "Sum"
+        evt.waveforms[contact_id]
+    end
+end
