cf831a04104d7720612d6a07ff6ba553ba3792fd
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-09-02 17:56:40 +0200

    Move ChargeCloudModel plot recipes to `src/PlotRecipes`

---
+++ src/PlotRecipes/ChargeCloudModels.jl
@@ -0,0 +1,261 @@
+@recipe function f(t::PointCharge{T}) where {T}
+
+    seriescolor --> :blue
+    
+    @series begin
+        seriescolor --> seriescolor
+        seriestype --> :scatter
+        markersize --> 6
+        label --> "PointCharge"
+        t.points
+    end
+end
+
+
+@recipe function f(nbc::NBodyChargeCloud{T}; connect = true, markersize = 10) where {T}
+    
+    seriescolor --> :blue
+    
+    @series begin
+        seriescolor --> seriescolor
+        connect --> connect
+        seriestype --> :scatter
+        markersize --> markersize
+        markerstrokewidth --> 0
+        label --> "NBodyChargeCloud"
+        []
+    end
+    
+    points = nbc.points
+    vertex_no = 0
+    for (shell, shell_structure) in enumerate(nbc.shell_structure)
+        vertices = get_vertices(shell_structure)
+        @series begin
+            markersize --> markersize * exp(-(shell - 1))
+            label --> ""
+            connect --> connect
+            seriescolor --> seriescolor
+            shell_structure(points[vertex_no+1:vertex_no+vertices])
+        end
+        vertex_no += vertices
+    end
+end
+
+
+@recipe function f(t::Tetrahedron{T}; connect = true) where {T}
+    
+    linestyle --> :dot
+    seriescolor --> :blue
+    
+    @series begin
+        seriescolor --> seriescolor
+        seriestype --> :scatter
+        markersize --> 6
+        label --> "Tetrahedron"
+        t.points
+    end
+    
+   
+    if connect
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            t.points[vcat(1,2,3,4,2)]
+        end
+
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            t.points[vcat(3,1,4)]
+        end
+    end
+end
+
+
+@recipe function f(i::Octahedron{T}; connect = true) where {T}
+    
+    linestyle --> :dot
+    seriescolor --> :blue
+    
+    @series begin
+        seriescolor --> seriescolor
+        seriestype --> :scatter
+        markersize --> 6
+        label --> "Octahedron"
+        i.points
+    end
+    
+    if connect    
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(1,2,3,4,5,2,6,5,1,3,6,4,1)]
+        end
+    end
+end
+
+
+@recipe function f(i::Hexahedron{T}; connect = true) where {T}
+    
+    linestyle --> :dot
+    seriescolor --> :blue
+    
+    @series begin
+        seriescolor --> seriescolor
+        seriestype --> :scatter
+        markersize --> 6
+        label --> "Hexahedron"
+        i.points
+    end
+    
+   
+    if connect
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(1,2,5,3,6,4,7,8,5)]
+        end
+
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(3,1,4)]
+        end
+
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(2,7)]
+        end
+
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(6,8)]
+        end
+    end
+end
+
+
+@recipe function f(i::Icosahedron{T}; connect = true) where {T}
+    
+    linestyle --> :dot
+    seriescolor --> :blue
+    
+    @series begin
+        seriescolor --> seriescolor
+        seriestype --> :scatter
+        markersize --> 6
+        label --> "Icosahedron"
+        i.points
+    end
+    
+   
+    if connect
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(1,2,7,8,9,10,11,7,3,8,4,9,5,10,6,11,2,3,4,5,6,2)]
+        end
+
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(3,1,6)]
+        end
+
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(5,1,4)]
+        end
+
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(7,12,8)]
+        end
+        
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(9,12,10)]
+        end
+        
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(11,12)]
+        end
+    end
+end
+
+
+@recipe function f(i::Dodecahedron{T}; connect = true) where {T}
+    
+    linestyle --> :dot
+    seriescolor --> :blue
+    
+    @series begin
+        seriescolor --> seriescolor
+        seriestype --> :scatter
+        markersize --> 6
+        label --> "Dodecahedron"
+        i.points
+    end
+    
+   
+    if connect
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(1,2,3,4,5,1,6,11,7,12,8,13,9,14,10,15,6)]
+        end
+
+        @series begin
+            seriescolor --> seriescolor
+            seriestype --> :line3d
+            label --> ""
+            linewidth --> 1
+            i.points[vcat(15,20,16,17,18,19,20)]
+        end
+        
+        for j in [2,3,4,5,11,12,13,14]
+            @series begin
+                seriescolor --> seriescolor
+                seriestype --> :line3d
+                label --> ""
+                linewidth --> 1
+                i.points[vcat(j,j+5)]
+            end
+        end
+    end
+end
