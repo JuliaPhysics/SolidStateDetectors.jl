49bb6f585b00b7c38451993eaa7ec27b350d90c2
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-10-12 11:36:24 +0200

    Update docstring for `Event`

--- src/Event/Event.jl
+++ src/Event/Event.jl
@@ -6,8 +6,8 @@ This (mutable) struct is meant to be used to look at individual events,
 not to process a huge amount of events.
 
 ## Fields
-* `locations::Union{VectorOfArrays{CartesianPoint{T}}, Missing}`: Vector of the positions of all hits of the event.
-* `energies::Union{VectorOfArrays{T}, Missing}`: Vector of energies corresponding to the hits of the event.
+* `locations::VectorOfArrays{CartesianPoint{T}}`: Vector of the positions of all hits of the event.
+* `energies::VectorOfArrays{T}`: Vector of energies corresponding to the hits of the event.
 * `drift_paths::Union{Vector{EHDriftPath{T}}, Missing}`: Calculated drift paths of each hit position. 
 * `waveforms::Union{Vector{<:Any}, Missing}`: Generated signals (waveforms) of the event.
 """
