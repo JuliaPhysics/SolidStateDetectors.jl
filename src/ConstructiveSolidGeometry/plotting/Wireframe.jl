9b87090c549c048e962c6528ecdd375b6fbc53bd
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2021-10-19 19:00:38 +0200

    Remove obsolete plotting code in CSG

--- src/ConstructiveSolidGeometry/plotting/Wireframe.jl
+++
@@ -1,7 +0,0 @@
-function get_plot_points(v::AbstractVolumePrimitive{T}; n = 30) where {T <: AbstractFloat}
-    plot_points = Vector{CartesianPoint{T}}[]
-    for surf in get_decomposed_surfaces(v)
-        append!(plot_points, get_plot_points(surf, n = n))
-    end
-    unique(plot_points)
-end
