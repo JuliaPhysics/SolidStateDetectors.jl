0745d3a7b53fb9a8e61a0352ecc9ffe65dcaebb1
Lukas Hauertmann <lhauert@mpp.mpg.de>
2022-02-15 15:07:52 +0100

    Add doc for `GeometricalRadialAxisWeights`

--- src/PotentialSimulation/GeometricalWeights/GeometricalWeights.jl
+++ src/PotentialSimulation/GeometricalWeights/GeometricalWeights.jl
@@ -86,6 +86,33 @@ struct GeometricalAzimutalAxisWeights{T} <: AbstractGeometricalAxisWeights{T}
     weights::Array{T, 2}
 end
 
+"""
+    struct GeometricalRadialAxisWeights{T} <: AbstractGeometricalAxisWeights{T}
+
+`GeometricalRadialAxisWeights` stores certain precalculated (in its construction) fixed values
+of the radial axis, `ax`, of an cylindrical grid on which the field calculation is performed. 
+These precalculated values are further used in the field calculation 
+to calculate the six weights for the neighboring grid points in the SOR. 
+
+`GeometricalRadialAxisWeights` has only one field: `weights::Array{T}, 2`, which is 
+of size `(6, length(ax.ticks))`. Each row holding the 6 precalculated values for one axis tick.
+
+Axis ticks, `t = ax.ticks`, and midpoints, `mp = midpoints(get_extended_ticks(ax))`,\\
+(the middle points between to axis ticks) inbetween:\\
+`... t[i-1] --- mp[i] --- t[i] --- mp[i+1] --- t[i+1] ...`\\
+are required for the understanding of the precalculated values.
+
+The columns store the following quantities:
+* `weights[1, i]`: `(mp[i+1] - t[i])` / `weights[3, i]`
+* `weights[2, i]`: `(t[i] - mp[i])` / `weights[3, i]`
+* `weights[3, i]`: `(mp[i+1] - mp[i]) / t[i]`
+* `weights[4, i]`: `mp[i+1] / (t[i+1] - t[i])`
+* `weights[5, i]`: `mp[i] / (t[i] - t[i-1])`\\
+  # Developer note: maybe this one could be removed as it basically stores the same as `weights[4, i-1]`
+* `weights[6, i]`: `(mp[i+1]^2 - mp[i]^2)/2`
+
+Developer note: Some of the weights for `i == 1` are set manually as `r = 0` requires some special treatment.
+"""
 struct GeometricalRadialAxisWeights{T} <: AbstractGeometricalAxisWeights{T}
     weights::Array{T, 2}
 end
