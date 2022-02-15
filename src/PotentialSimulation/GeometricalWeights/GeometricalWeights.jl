613e8c820d8a757edb1c2f528265f3eaa44a4412
Lukas Hauertmann <lhauert@mpp.mpg.de>
2022-02-15 15:52:09 +0100

    Remove unused constructor method

--- src/PotentialSimulation/GeometricalWeights/GeometricalWeights.jl
+++ src/PotentialSimulation/GeometricalWeights/GeometricalWeights.jl
@@ -35,27 +35,6 @@ struct GeometricalCartesianAxisWeights{T} <: AbstractGeometricalAxisWeights{T}
     weights::Array{T, 2}
 end
 
-function GeometricalCartesianAxisWeights( ax::DiscreteAxis{T, BL, BR} )::GeometricalCartesianAxisWeights{T} where {T, BL, BR}
-    axticks::Vector{T} = collect(ax.ticks)
-    ax_ext::Vector{T} = get_extended_ticks(ax)
-    Δax_ext::Vector{T} = diff(ax_ext)
-    Δax_ext_inv::Vector{T} = inv.(Δax_ext)
-    mpax::Vector{T} = midpoints(ax_ext)
-    Δmpax::Vector{T} = diff(mpax)
-    Δmpax_inv::Vector{T} = inv.(Δmpax)
-    Δhmpaxr::Vector{T} = mpax[2:end] - axticks
-    Δhmpaxl::Vector{T} = axticks - mpax[1:end - 1]
-    waxr::Vector{T} = Δmpax_inv .* Δhmpaxr
-    waxl::Vector{T} = Δmpax_inv .* Δhmpaxl
-
-    w::Array{T, 2} = zeros(T, 4, length(waxr) + 1)
-    w[1, 1:length(waxr)] = waxr
-    w[2, 1:length(waxr)] = waxl
-    w[3, 1:length(waxr)] = Δmpax
-    w[4, :] = Δax_ext_inv
-
-    return GeometricalCartesianAxisWeights{T}( w )
-end
 
 """
     struct GeometricalAzimutalAxisWeights{T} <: AbstractGeometricalAxisWeights{T}
