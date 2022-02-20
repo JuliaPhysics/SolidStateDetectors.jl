68c8aee0a1dcd77afa202d6975f8bacbb70cc146
Lukas Hauertmann <lhauert@mpp.mpg.de>
2022-02-20 14:21:37 +0100

    Add first convergence based on average change

--- src/PotentialCalculation/SuccessiveOverRelaxation/GPU_convergence.jl
+++ src/PotentialCalculation/SuccessiveOverRelaxation/GPU_convergence.jl
@@ -10,24 +10,56 @@ function _update_till_convergence!( pssrb::PotentialCalculationSetup{T, S, 3},
                                     verbose::Bool = true
                                 ) where {T, S, depletion_handling_enabled, only_2d, _is_weighting_potential, DAT <: GPUArrays.AbstractGPUArray}
     device = get_device(DAT)
-    N_grid_points = prod(size(pssrb.potential)[1:3] .- 2)
+    rb_half_inner_size = size(pssrb.potential)[1:3] .- 2
+    ndrange = prod(rb_half_inner_size)
     kernel = get_sor_kernel(S, device)
-    @showprogress for i in 1:max_n_iterations
-        update_even_points = true
-        wait(kernel( 
-            pssrb.potential, pssrb.point_types, pssrb.volume_weights, pssrb.q_eff_imp, pssrb.q_eff_fix, pssrb.ϵ_r,
-            pssrb.geom_weights, pssrb.sor_const, update_even_points, depletion_handling_enabled, _is_weighting_potential, only_2d, 
-            ndrange = N_grid_points
-        ))
-        apply_boundary_conditions!(pssrb, Val(update_even_points), only2d)
-        update_even_points = false
-        wait(kernel( 
-            pssrb.potential, pssrb.point_types, pssrb.volume_weights, pssrb.q_eff_imp, pssrb.q_eff_fix, pssrb.ϵ_r,
-            pssrb.geom_weights, pssrb.sor_const, update_even_points, depletion_handling_enabled, _is_weighting_potential, only_2d,
-            ndrange = N_grid_points
-        ))
-        apply_boundary_conditions!(pssrb, Val(update_even_points), only2d)
+    c_limit = _is_weighting_potential ? convergence_limit : abs(convergence_limit * pssrb.bias_voltage)
+    c = c_limit * 10
+    n_performed_iterations = 0
+    tmp_potential = similar(pssrb.potential, rb_half_inner_size)
+    n_half_grid_points = length(tmp_potential)
+    inner_ranges = broadcast(i -> 2:size(tmp_potential, i) + 1, (1, 2, 3))
+    is_logging(io) = isa(io, Base.TTY) == false || (get(ENV, "CI", nothing) == "true")
+    if verbose prog = ProgressThresh(c; dt = 0.1, desc = "Convergence: ", output = stderr, enabled = !is_logging(stderr)) end
+    while c > c_limit
+        for k in 1:n_iterations_between_checks-1
+            update!(pssrb, kernel, ndrange, depletion_handling_enabled, _is_weighting_potential, only2d)
+            n_performed_iterations += 1
+        end
+        begin
+            tmp_potential[:, :, :] .= view(pssrb.potential, inner_ranges..., 1)
+            update!(pssrb, kernel, ndrange, depletion_handling_enabled, _is_weighting_potential, only2d)
+            tmp_potential[:, :, :] .-= view(pssrb.potential, inner_ranges..., 1)
+            c = sum(abs.(tmp_potential)) / n_half_grid_points
+            if verbose ProgressMeter.update!(prog, c) end
+            n_performed_iterations += 1
+        end
+        if n_performed_iterations >= max_n_iterations break end
     end
-    return 0
+    if verbose ProgressMeter.finish!(prog) end
+    return c
 end                
 
+@inline function update!(
+    pssrb::PotentialCalculationSetup{T, S, 3},
+    kernel, ndrange,
+    depletion_handling_enabled, 
+    _is_weighting_potential, 
+    only2d::Val{only_2d}
+) where {T, S, only_2d}
+    update_even_points = true
+    wait(kernel( 
+        pssrb.potential, pssrb.point_types, pssrb.volume_weights, pssrb.q_eff_imp, pssrb.q_eff_fix, pssrb.ϵ_r,
+        pssrb.geom_weights, pssrb.sor_const, update_even_points, depletion_handling_enabled, _is_weighting_potential, only_2d, 
+        ndrange = ndrange
+    ))
+    apply_boundary_conditions!(pssrb, Val(update_even_points), only2d)
+    update_even_points = false
+    wait(kernel( 
+        pssrb.potential, pssrb.point_types, pssrb.volume_weights, pssrb.q_eff_imp, pssrb.q_eff_fix, pssrb.ϵ_r,
+        pssrb.geom_weights, pssrb.sor_const, update_even_points, depletion_handling_enabled, _is_weighting_potential, only_2d,
+        ndrange = ndrange
+    ))
+    apply_boundary_conditions!(pssrb, Val(update_even_points), only2d)
+    return nothing
+end
