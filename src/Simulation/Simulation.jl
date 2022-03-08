1e055fb717c793ad4a9db3a15991d4556ecee30d
Lukas Hauertmann <lhauert@mpp.mpg.de>
2022-03-08 17:20:23 +0100

    Don't set `imp_scale` to 0 in contacts

--- src/Simulation/Simulation.jl
+++ src/Simulation/Simulation.jl
@@ -964,7 +964,7 @@ function _calculate_potential!( sim::Simulation{T, CS}, potential_type::UnionAll
     
     if isEP mark_bulk_bits!(sim.point_types.data) end
     if depletion_handling && isEP
-        if isEP mark_undep_bits_and_contact_imp_scale!(sim.point_types.data, sim.imp_scale.data) end
+        mark_undep_bits!(sim.point_types.data, sim.imp_scale.data)
     end
     
     nothing
