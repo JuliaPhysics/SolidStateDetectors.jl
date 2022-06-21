86592254a9b505fff1c8299c26abe32cee7ea2a8
Sebastian Ruffert <ruffert@mpp.mpg.de>
2022-06-21 11:03:31 +0200

    Add convenient constructor for `Line`

--- src/ConstructiveSolidGeometry/LinePrimitives/Line.jl
+++ src/ConstructiveSolidGeometry/LinePrimitives/Line.jl
@@ -1,6 +1,27 @@
-struct Line{T} <: AbstractLinePrimitive{T}
+struct Line{T<:AbstractFloat} <: AbstractLinePrimitive{T}
     origin::CartesianPoint{T}
     direction::CartesianVector{T}
+end
+
+#Type promotion happens here
+function Line(origin::PT, direction::DIR) where {PT<:(CartesianPoint), DIR<:(CartesianVector)}
+    eltypes = _csg_get_promoted_eltype.((PT, DIR))
+    T = float(promote_type(eltypes...))
+    Line{T}(origin, direction)
+end
+
+function Line(;
+    origin = zero(CartesianPoint{Int}), 
+    direction = CartesianVector{Int}(0,0,1)
+) 
+    Line(origin, direction)
+end
+
+function Line{T}(;
+    origin = zero(CartesianPoint{Float64}), 
+    direction = CartesianVector{Float64}(0,0,1)
+) where {T}
+    Line{T}(origin, direction)
 end
 
 distance(pt::CartesianPoint, l::Line) = norm((pt - l.origin) × l.direction) / norm(l.direction)
