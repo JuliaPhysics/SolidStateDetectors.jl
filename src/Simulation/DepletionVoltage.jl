12fb22f6328caa8c88b8726a20f4e68fb6edec49
Lukas Hauertmann <lhauert@mpp.mpg.de>
2022-09-02 09:32:10 +0200

    Simplify `estimate_depletion_voltage`

--- src/Simulation/DepletionVoltage.jl
+++ src/Simulation/DepletionVoltage.jl
@@ -22,8 +22,7 @@ end
     estimate_depletion_voltage( sim::Simulation{T}, contact_id::Int, field_sim_settings = (verbose = true,))::T
 
 Estimates the full depletion voltage, U_D, of a detector in a given [`Simulation`](@ref).\\
-The `contact_id` at which the bias voltage is applied has to be specified.\\
-to fully deplete the detector.\\
+That is is the voltage to fully deplete the detector.\\
 This is done by calculating two electric potentials with different boundary conditions:\\
     1) Only the electric potential coming from the impurity density alone: `EP_i`\\
     2) The electric potential without an impurity density: `EP_0`\\
@@ -32,12 +31,13 @@ to determine `U_D` which is the voltage where the gradient of the suposition fie
 becomes nowhere 0 anywhere inside the semiconductor. 
 
 ## Arguments 
-* `sim::Simulation{T}`: [`Simulation`](@ref) for which the depletion voltage should be determined.
-* `contact_id::Int`: The `id` of the [`Contact`](@ref) at which the potential is applied.
-    
+* `sim::Simulation{T}`: [`Simulation`](@ref) of the detector for which the depletion voltage should be determined.
+
 ## Keywords
+* `bias_voltage_contact_id::Int`: The `id` of the [`Contact`](@ref) at which the potential is applied.\\
+As default it is tried to determine this automatically via `determine_bias_voltage_contact_id(sim.detector)`.
 * `field_sim_settings::NamedTuple = (verbose = false,)`: NamedTuple of simulation settings merged with the\\
-    default settings (listed underneath) and passed further to the field calculation functions.
+default settings (listed underneath) and passed further to the field calculation functions.
 
 The following default settings are used for the field simulations performed in this function:
 ```julia
@@ -56,7 +56,7 @@ The following default settings are used for the field simulations performed in t
 ```julia  
 using SolidStateDetectors
 sim = Simulation(SSD_examples[:InvertedCoax])
-estimate_depletion_voltage(sim, 2, field_sim_settings = (verbose = true,))
+estimate_depletion_voltage(sim, field_sim_settings = (verbose = true,))
 ```
 
 !!! note
@@ -70,13 +70,13 @@ See also [`is_depleted`](@ref).
 """
 function estimate_depletion_voltage(
         sim::Simulation{T}, 
-        contact_id::Int,
-        field_sim_settings::NamedTuple = (verbose = false,)
+        field_sim_settings::NamedTuple = (verbose = false,);
+        bias_voltage_contact_id::Int = determine_bias_voltage_contact_id(sim.detector)
     ) where {T <: AbstractFloat}
     
     simDV = Simulation{T}(sim.config_dict)
 
-    simDV.detector = SolidStateDetector(simDV.detector, contact_id = contact_id, contact_potential = 0)
+    simDV.detector = SolidStateDetector(simDV.detector, contact_id = bias_voltage_contact_id, contact_potential = 0)
 
     max_threads = Base.Threads.nthreads()
 
@@ -91,44 +91,49 @@ function estimate_depletion_voltage(
     ), field_sim_settings)
 
     calculate_electric_potential!(simDV; fss...)
-    calculate_weighting_potential!(simDV, contact_id; fss...)
-    _adapt_weighting_potential_to_electric_potential_grid!(simDV, contact_id)
+    calculate_weighting_potential!(simDV, bias_voltage_contact_id; fss...)
+    _adapt_weighting_potential_to_electric_potential_grid!(simDV, bias_voltage_contact_id)
     
-    grid_size = size(simDV.point_types.grid)
-    depletion_voltage_field = zeros(T, grid_size)
-
+    estimate_depletion_voltage(
+        simDV.weighting_potentials[bias_voltage_contact_id],
+        simDV.electric_potential,
+        simDV.point_types, 
+    )
+end
+        
+function estimate_depletion_voltage(
+    zero_imp_ep::ScalarPotential{T}, 
+    only_imp_ep::ScalarPotential{T}, 
+    point_types::PointTypes
+    ) where {T}
+    depletion_voltage_field = zeros(T, size(point_types.grid))
     _estimate_depletion_voltage_factor!(
         depletion_voltage_field, 
-        grid_size, 
-        simDV.point_types, 
-        simDV.electric_potential.data, 
-        simDV.weighting_potentials[contact_id].data 
+        point_types,
+        only_imp_ep.data,
+        zero_imp_ep.data
     )
-
-    depletion_voltage = maximum(depletion_voltage_field) * u"V"
-
-    # estimated_uncertainty = fss.refinement_limits[end] * depletion_voltage
-    return depletion_voltage
+    maximum(depletion_voltage_field) * u"V" # full depletion voltage
 end
 
 
 function _estimate_depletion_voltage_factor(
-        center_imp_value::T, 
+        center_only_imp_value::T, 
         center_zero_imp_value::T, 
-        neighbor_imp_values::AbstractVector{T}, 
+        neighbor_only_imp_values::AbstractVector{T}, 
         neighbor_zero_imp_values::AbstractVector{T}
     ) where {T}
-    estimation = abs(center_imp_value) / center_zero_imp_value 
-    neighbor_values = neighbor_zero_imp_values .* estimation .+ neighbor_imp_values
+    estimation = abs(center_only_imp_value) / center_zero_imp_value 
+    neighbor_values = neighbor_zero_imp_values .* estimation .+ neighbor_only_imp_values
     vmin, vmax = extrema(neighbor_values)
-    center_value = center_zero_imp_value * estimation + center_imp_value
+    center_value = center_zero_imp_value * estimation + center_only_imp_value
     if center_value < vmin
         estimation += (vmin - center_value) / center_zero_imp_value 
     elseif center_value > vmax
         estimation -= (vmax - center_value) / center_zero_imp_value
     end
-    # neighbor_values = neighbor_zero_imp_values .* estimation .+ neighbor_imp_values
-    # center_value = center_zero_imp_value * estimation + center_imp_value
+    # neighbor_values = neighbor_zero_imp_values .* estimation .+ neighbor_only_imp_values
+    # center_value = center_zero_imp_value * estimation + center_only_imp_value
     # vmin, vmax = extrema(neighbor_values)
     # @assert (center_value < vmin || center_value > vmax) 
     return estimation
@@ -141,37 +146,37 @@ end
 
 function _estimate_depletion_voltage_factor!(
         depletion_voltage_field::AbstractArray{T}, 
-        grid_size, 
-        pts, 
-        imp_field, 
-        zero_imp_field
+        point_types, 
+        only_imp_ep, 
+        zero_imp_ep
     ) where {T}
     @inbounds begin
+        grid_size = size(point_types)
         for i3 in 1:grid_size[3]
             for i2 in 1:grid_size[2]
                 for i1 in 1:grid_size[1]
-                    if is_pn_junction_point_type(pts[i1, i2, i3])
-                        center_imp_value = imp_field[i1, i2, i3]
-                        center_zero_imp_value = zero_imp_field[i1, i2, i3]
-                        neighbor_imp_values::SVector{6, T} = _replace_NaN_with_minimum(@SVector T[
-                            i1 < grid_size[1] && is_pn_junction_point_type(pts[i1+1, i2, i3]) ? imp_field[i1+1, i2, i3] : T(NaN),
-                            i1 > 1 && is_pn_junction_point_type(pts[i1-1, i2, i3]) ? imp_field[i1-1, i2, i3] : T(NaN),
-                            i2 < grid_size[2] && is_pn_junction_point_type(pts[i1, i2+1, i3]) ? imp_field[i1, i2+1, i3] : T(NaN),
-                            i2 > 1 && is_pn_junction_point_type(pts[i1, i2-1, i3]) ? imp_field[i1, i2-1, i3] : T(NaN),
-                            i3 < grid_size[3] && is_pn_junction_point_type(pts[i1, i2, i3+1]) ? imp_field[i1, i2, i3+1] : T(NaN),
-                            i3 > 1 && is_pn_junction_point_type(pts[i1, i2, i3-1]) ? imp_field[i1, i2, i3-1] : T(NaN)
+                    if is_pn_junction_point_type(point_types[i1, i2, i3])
+                        center_only_imp_value = only_imp_ep[i1, i2, i3]
+                        center_zero_imp_value = zero_imp_ep[i1, i2, i3]
+                        neighbor_only_imp_values::SVector{6, T} = _replace_NaN_with_minimum(@SVector T[
+                            i1 < grid_size[1] && is_pn_junction_point_type(point_types[i1+1, i2, i3]) ? only_imp_ep[i1+1, i2, i3] : T(NaN),
+                            i1 > 1 && is_pn_junction_point_type(point_types[i1-1, i2, i3]) ? only_imp_ep[i1-1, i2, i3] : T(NaN),
+                            i2 < grid_size[2] && is_pn_junction_point_type(point_types[i1, i2+1, i3]) ? only_imp_ep[i1, i2+1, i3] : T(NaN),
+                            i2 > 1 && is_pn_junction_point_type(point_types[i1, i2-1, i3]) ? only_imp_ep[i1, i2-1, i3] : T(NaN),
+                            i3 < grid_size[3] && is_pn_junction_point_type(point_types[i1, i2, i3+1]) ? only_imp_ep[i1, i2, i3+1] : T(NaN),
+                            i3 > 1 && is_pn_junction_point_type(point_types[i1, i2, i3-1]) ? only_imp_ep[i1, i2, i3-1] : T(NaN)
                         ])
                         neighbor_zero_imp_values::SVector{6, T} = _replace_NaN_with_minimum(@SVector T[
-                            i1 < grid_size[1] && is_pn_junction_point_type(pts[i1+1, i2, i3]) ? zero_imp_field[i1+1, i2, i3] : T(NaN),
-                            i1 > 1 && is_pn_junction_point_type(pts[i1-1, i2, i3]) ? zero_imp_field[i1-1, i2, i3] : T(NaN),
-                            i2 < grid_size[2] && is_pn_junction_point_type(pts[i1, i2+1, i3]) ? zero_imp_field[i1, i2+1, i3] : T(NaN),
-                            i2 > 1 && is_pn_junction_point_type(pts[i1, i2-1, i3]) ? zero_imp_field[i1, i2-1, i3] : T(NaN),
-                            i3 < grid_size[3] && is_pn_junction_point_type(pts[i1, i2, i3+1]) ? zero_imp_field[i1, i2, i3+1] : T(NaN),
-                            i3 > 1 && is_pn_junction_point_type(pts[i1, i2, i3-1]) ? zero_imp_field[i1, i2, i3-1] : T(NaN)
+                            i1 < grid_size[1] && is_pn_junction_point_type(point_types[i1+1, i2, i3]) ? zero_imp_ep[i1+1, i2, i3] : T(NaN),
+                            i1 > 1 && is_pn_junction_point_type(point_types[i1-1, i2, i3]) ? zero_imp_ep[i1-1, i2, i3] : T(NaN),
+                            i2 < grid_size[2] && is_pn_junction_point_type(point_types[i1, i2+1, i3]) ? zero_imp_ep[i1, i2+1, i3] : T(NaN),
+                            i2 > 1 && is_pn_junction_point_type(point_types[i1, i2-1, i3]) ? zero_imp_ep[i1, i2-1, i3] : T(NaN),
+                            i3 < grid_size[3] && is_pn_junction_point_type(point_types[i1, i2, i3+1]) ? zero_imp_ep[i1, i2, i3+1] : T(NaN),
+                            i3 > 1 && is_pn_junction_point_type(point_types[i1, i2, i3-1]) ? zero_imp_ep[i1, i2, i3-1] : T(NaN)
                         ])
 
                         depletion_voltage_field[i1, i2, i3] = _estimate_depletion_voltage_factor(
-                            center_imp_value, center_zero_imp_value, neighbor_imp_values, neighbor_zero_imp_values
+                            center_only_imp_value, center_zero_imp_value, neighbor_only_imp_values, neighbor_zero_imp_values
                         )
                     end
                 end
