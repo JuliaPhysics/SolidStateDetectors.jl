64b026538079dd9e3cc3d51db769c62f4bed1a17
Felix Hagemann <hagemann@mpp.mpg.de>
2023-06-12 21:06:09 +0200

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,7 +31,7 @@ T = Float32
 @testset "Test real detectors" begin
     @testset "Simulate example detector: Inverted Coax" begin
         sim = Simulation{T}(SSD_examples[:InvertedCoax])
-        simulate!(sim, convergence_limit = 1e-6, device_array_type = device_array_type, refinement_limits = [0.2, 0.1], verbose = false)
+        simulate!(sim, convergence_limit = 1e-6, device_array_type = device_array_type, refinement_limits = [0.2, 0.1, 0.05, 0.03, 0.02, 0.01], verbose = false)
         evt = Event(CartesianPoint.([CylindricalPoint{T}(20e-3, deg2rad(10), 10e-3 )]))
         simulate!(evt, sim, Δt = 1e-9, max_nsteps = 10000)
         signalsum = T(0)
@@ -43,7 +43,7 @@ T = Float32
         @test isapprox( signalsum, T(2), atol = 5e-3 )
         nt = NamedTuple(sim)
         @test sim == Simulation(nt)
-        deplV = estimate_depletion_voltage(sim, (verbose = false,))
+        deplV = estimate_depletion_voltage(sim, verbose = false)
         @test isapprox(deplV, 1870*u"V", atol = 5.0*u"V") 
         id = SolidStateDetectors.determine_bias_voltage_contact_id(sim.detector)
         # Check wether detector is undepleted, 10V below the previously calculated depletion voltage
@@ -117,11 +117,11 @@ T = Float32
             l = vcat(wp.data[rmin:-1:2,φidx2,zidx], wp.data[1:rmax,φidx1,zidx])
             @test all(diff(l) .< 0) # the weighting potential should monotonously decrease from rmin to rmax
         end
-        @test isapprox(estimate_depletion_voltage(sim, (verbose = false,)), 0u"V", atol = 0.2u"V") # This detector has no impurity profile
+        @test isapprox(estimate_depletion_voltage(sim, verbose = false), 0u"V", atol = 0.2u"V") # This detector has no impurity profile
     end
     @testset "Simulate example detector: HexagonalPrism" begin
         sim = Simulation{T}(SSD_examples[:Hexagon])
-        simulate!(sim, convergence_limit = 1e-6, device_array_type = device_array_type, refinement_limits = [0.2, 0.1], verbose = false)
+        simulate!(sim, convergence_limit = 1e-6, device_array_type = device_array_type, refinement_limits = [0.2, 0.1, 0.05, 0.02], verbose = false)
         evt = Event([CartesianPoint{T}(0, 5e-4, 1e-3)])
         simulate!(evt, sim, Δt = 1e-9, max_nsteps = 10000)
         signalsum = T(0)
@@ -131,7 +131,7 @@ T = Float32
         signalsum *= inv(ustrip(SolidStateDetectors._convert_internal_energy_to_external_charge(sim.detector.semiconductor.material)))
         @info signalsum
         @test isapprox( signalsum, T(2), atol = 5e-3 )
-        @test isapprox(estimate_depletion_voltage(sim, (verbose = false,)), T(-13.15)*u"V", atol = 1.0u"V") 
+        @test isapprox(estimate_depletion_voltage(sim, verbose = false), T(-13.15)*u"V", atol = 1.0u"V") 
     end
     @testset "Simulate example detector: CGD" begin
         sim = Simulation{T}(SSD_examples[:CGD])
