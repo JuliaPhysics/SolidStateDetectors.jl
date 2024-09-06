b954dd762fa34349c0208514e70cd63595320634
David Hervas Aguilar <41748838+hervasa2@users.noreply.github.com>
2024-09-06 11:29:17 +0200

    2D slice plotting (#405)
    * 2D projection plotting
    
    * cleanup
    
    * bug fixes
    
    * axis labels
    
    * eliminate plotting redundancies
    
    * phi slice support
    
    * slice plotting front end
    
    * projection and additional keyword support
    
    * plotting docs
    
    * tutorial
    
    ---------
    
    Co-authored-by: David Hervas <davidhervas@Davids-MBP-14.fritz.box>
    Co-authored-by: David Hervas <davidhervas@Davids-MacBook-Pro-14.local>

--- src/ConstructiveSolidGeometry/plotting/CSG/CSG.jl
+++ src/ConstructiveSolidGeometry/plotting/CSG/CSG.jl
@@ -6,59 +6,81 @@
     )
 end
 
-@recipe function f(csg::AbstractConstructiveGeometry{T}; n_samples = 40, CSG_scale = missing) where {T}
-    seriestype --> :csg
-    xunit --> internal_length_unit
-    yunit --> internal_length_unit
-    zunit --> internal_length_unit
-    ps = primitives(csg)
-    if haskey(plotattributes, :seriestype) && plotattributes[:seriestype] == :samplesurface
-        spacing::T = T((ismissing(CSG_scale) ? get_scale(csg) : CSG_scale)/n_samples)
+@recipe function f(csg::AbstractConstructiveGeometry{T}, primitive::AbstractPrimitive{T}, axis::Symbol, slice::T, st::Symbol; n_samples = 40, CSG_scale = missing, projection = :none, full_det = false, linewidth = :auto) where {T}
+    if st == :samplesurface
+        CSG_scale = ismissing(CSG_scale) ? get_scale(csg) : CSG_scale
+        spacing = T(CSG_scale/n_samples)
+        filter(p -> in(p,csg), sample(primitive, spacing))
+    elseif st == :slice
+        isgr = occursin("GRBackend", string(typeof(plotattributes[:plot_object].backend)))
+        CSG_scale = ismissing(CSG_scale) ? get_scale(csg) : CSG_scale
+        pointtype, axisrot = CartesianPoint, axis
+        spacing = T(CSG_scale/n_samples)
+        if axis == :φ
+            pointtype, axisrot, slice, spacing = CylindricalPoint, :y, 0, spacing*T(0.5)
+        end
+        samples = filter(pt -> in(pt, csg) && abs(getproperty(pt, axisrot) - slice) < spacing/2, sample(primitive, spacing))
+        proj = fieldnames(CartesianPoint)[findall(x -> x != axis, fieldnames(pointtype))]
+        u = getproperty.(samples, proj[1])
+        v = getproperty.(samples, proj[2])
+        if axis == :φ && !full_det
+            idx = findall(0 .≤ u)
+            u = u[idx]
+            v = v[idx]
+        end
+        proj = filter(x -> x != axis, fieldnames(pointtype))
+        yunit --> internal_length_unit
+        xguide --> string(proj[1])
+        yguide --> string(proj[2])
+        if linewidth != :auto
+            markersize := linewidth
+        end
+        if projection == :polar && axis == :z
+            xunit --> internal_angle_unit
+            uconvert.(internal_angle_unit, atan.(v,u)), internal_length_unit*sqrt.(u.^2 + v.^2)
+        else
+            if isgr aspect_ratio --> 1.0 end
+            xunit --> internal_length_unit
+            internal_length_unit*u, internal_length_unit*v
+        end
+    else
+        if !isClosedPrimitive(primitive)
+            fillcolor := :white
+            fillalpha --> 0.2
+            linewidth := 1
+        end
+        primitive
     end
+end
+
+@recipe function f(csg::AbstractConstructiveGeometry{T}; x = missing, y = missing, z = missing, φ = missing) where {T}
+    seriestype --> :csg
+    st = plotattributes[:seriestype]
+    unitformat --> :slash
+    axis, slice  = get_crosssection_and_value(x,y,z,φ)
+    if st == :slice
+        surfs = filter(s -> typeof(s) <: AbstractPlanarSurfacePrimitive{T}, surfaces(csg))
+        if any(s -> is_coplanar(s, (axis, T(slice))), surfs)
+            n_samples --> 100
+        else
+            n_samples --> 200
+        end
+        if axis == :φ
+            csg = transform(csg, (rotation = T.(RotZ(-to_internal_units(slice))), translation = CartesianVector{T}()))
+        end
+    else
+        n_samples --> 40
+    end
+    ps = primitives(csg)
+    
     @series begin
         label --> "CSG"
-        if haskey(plotattributes, :seriestype) 
-            if plotattributes[:seriestype] == :samplesurface
-                if occursin("GRBackend", string(typeof(plotattributes[:plot_object].backend)))
-                    aspect_ratio --> 1.0
-                end 
-                seriesalpha --> 0.2
-                filter(p -> in(p,csg), sample(ps[1], spacing))
-            else
-                if !isClosedPrimitive(ps[1])
-                    fillcolor := :white
-                    fillalpha --> 0.2
-                    if haskey(plotattributes, :seriestype) 
-                        if plotattributes[:seriestype] in [:csg, :wireframe] 
-                            linewidth := 1
-                        end
-                    end
-                end
-                ps[1]
-            end
-        end
+        csg, ps[1], axis, T(slice), st
     end
     for i in 2:length(ps)
         @series begin
             label := ""
-            if haskey(plotattributes, :seriestype) 
-                if plotattributes[:seriestype] == :samplesurface
-                    seriesalpha --> 0.2
-                    filter(p -> in(p,csg), sample(ps[i], spacing))
-                else
-                    if !isClosedPrimitive(ps[i])
-                        fillcolor := :white
-                        fillalpha --> 0.2
-                        if haskey(plotattributes, :seriestype) 
-                            if plotattributes[:seriestype] in [:csg, :wireframe] 
-                                linewidth := 1
-                            end
-                        end
-                    end
-                    ps[i]
-                end
-            end
+            csg, ps[i], axis, T(slice), st
         end
     end
 end
-
--- src/ConstructiveSolidGeometry/plotting/VolumePrimitives/VolumePrimitives.jl
+++ src/ConstructiveSolidGeometry/plotting/VolumePrimitives/VolumePrimitives.jl
@@ -7,17 +7,89 @@ function sample(p::AbstractVolumePrimitive{T}, spacing::T)::Vector{CartesianPoin
     vs
 end
 
-@recipe function f(p::AbstractVolumePrimitive; n_samples = 40)
+function get_crosssection_and_value(x::Union{Real, LengthQuantity, Missing}, y::Union{Real, LengthQuantity, Missing}, z::Union{Real, LengthQuantity, Missing}, φ::Union{Real, AngleQuantity, Missing})::Tuple{Symbol, Real}
+    crosssections = Dict(:x => x, :y => y, :z => z, :φ => φ)
+    idx = findfirst(cs -> !ismissing(cs), crosssections)
+    if isnothing(idx)
+        :y, 0
+    else
+        return idx, to_internal_units(crosssections[idx])
+    end
+end
+
+function is_coplanar(s::AbstractSurfacePrimitive{T}, plane::Tuple{Symbol,T}) where {T}
+    axis, slice = plane
+    crosssections = Dict(   :x => CartesianVector{T}(1,0,0), 
+                            :y => CartesianVector{T}(0,1,0), 
+                            :z => CartesianVector{T}(0,0,1),
+                            :φ => CartesianVector{T}(cos(π/2+slice),sin(π/2+slice),0)
+                   )
+    pointtype = axis == :φ ? CylindricalPoint : CartesianPoint
+    point = pointtype(vertices(s,1)[1])
+    abs(abs(dot(normal(s), crosssections[axis])) - 1) < csg_default_tol(T) && abs(getproperty(point, axis) - slice) < csg_default_tol(T)
+end
+
+@recipe function f(p::AbstractPrimitive{T}, axis::Symbol, slice::T, st::Symbol; n_samples = 40, CSG_scale = missing, projection = :none, full_det = false, linewidth = :auto) where {T}
+    if st == :samplesurface
+        CSG_scale = ismissing(CSG_scale) ? extremum(p) : CSG_scale
+        spacing = T(CSG_scale/n_samples)
+        sample(p, spacing)
+    elseif st == :slice
+        isgr = occursin("GRBackend", string(typeof(plotattributes[:plot_object].backend)))
+        pointtype, axisrot = CartesianPoint, axis
+        CSG_scale = ismissing(CSG_scale) ? extremum(p) : CSG_scale
+        spacing = T(CSG_scale/n_samples)
+        if axis == :φ
+            p = rotate(p, T.(RotZ(-to_internal_units(slice))))
+            pointtype, axisrot, slice, spacing = CylindricalPoint, :y, 0, spacing*T(0.5)
+        end
+        samples = filter(pt -> abs(getproperty(pt, axisrot) - T(to_internal_units(slice))) < spacing/2, sample(p, spacing))
+        proj = fieldnames(CartesianPoint)[findall(x -> x != axis, fieldnames(pointtype))]
+        u = getproperty.(samples, proj[1])
+        v = getproperty.(samples, proj[2])
+        if axis == :φ && !full_det
+            idx = findall(0 .≤ u)
+            u = u[idx]
+            v = v[idx]
+        end
+        proj = filter(x -> x != axis, fieldnames(pointtype))
+        yunit --> internal_length_unit
+        xguide --> string(proj[1])
+        yguide --> string(proj[2])
+        if linewidth != :auto
+            markersize := linewidth
+        end
+        if projection == :polar && axis == :z
+            xunit --> internal_angle_unit
+            uconvert.(internal_angle_unit, atan.(v,u)), internal_length_unit*sqrt.(u.^2 + v.^2)
+        else
+            if isgr aspect_ratio --> 1.0 end
+            xunit --> internal_length_unit
+            internal_length_unit*u, internal_length_unit*v
+        end
+    else
+        [surfaces(p)...]
+    end
+end
+
+@recipe function f(p::AbstractVolumePrimitive{T}; x = missing, y = missing, z = missing, φ = missing) where {T}
     linecolor --> :black
-    if occursin("GRBackend", string(typeof(plotattributes[:plot_object].backend)))
-        aspect_ratio --> 1.0
-    end 
+    seriestype --> :csg
+    st = plotattributes[:seriestype]
+    unitformat --> :slash
+    axis, slice  = get_crosssection_and_value(x,y,z,φ)
+    if st == :slice
+        surfs = filter(s -> typeof(s) <: AbstractPlanarSurfacePrimitive{T}, surfaces(p))
+        if any(s -> is_coplanar(s, (axis, T(slice))), surfs)
+            n_samples --> 100
+        else
+            n_samples --> 200
+        end
+    else
+        n_samples --> 40
+    end
     @series begin
         label --> "$(nameof(typeof(p)))"
-        if haskey(plotattributes, :seriestype) && plotattributes[:seriestype] == :samplesurface
-            sample(p, extremum(p)/n_samples)
-        else
-            [surfaces(p)...]
-        end
+        p, axis, T(slice), st
     end
 end
