6af3a1554fcc738a5a94c5ea19e5ea1f21e304df
David Hervas <davidhervas@Davids-MBP-14.fritz.box>
2024-09-06 10:02:09 +0200

    tutorial

--- docs/src/tutorials/complete_simulation_chain_IVC_lit.jl
+++ docs/src/tutorials/complete_simulation_chain_IVC_lit.jl
@@ -13,6 +13,19 @@ plot(sim.detector, xunit = u"mm", yunit = u"mm", zunit = u"mm", size = (700, 700
 #md savefig("tutorial_det.svg"); nothing # hide
 #md # [![tutorial_det](tutorial_det.svg)](tutorial_det.pdf)
 
+# By default only the contacts and passives are shown in detector plots. One can also take a look at the semiconductor. The following plots show the semiconductor and slices in the y and z direction. 
+
+plot(
+    plot(sim.detector.semiconductor, st = :samplesurface),
+    plot(sim.detector.semiconductor, st = :slice, y = 0u"mm", lw = 2),
+    plot(sim.detector.semiconductor, st = :slice, z = 40u"mm", lw = 2),
+    layout = (1,3), size = (1500,500), legend = false
+    )
+#jl savefig("tutorial_semiconductor.pdf") # hide
+#md savefig("tutorial_semiconductor.pdf") # hide
+#md savefig("tutorial_semiconductor.svg"); nothing # hide
+#md # [![tutorial_semiconductor](tutorial_semiconductor.svg)](tutorial_semiconductor.pdf)
+
 # One can also have a look at how the initial conditions look like on the grid (its starts with a very coarse grid):
 
 apply_initial_state!(sim, ElectricPotential) # optional
@@ -150,9 +163,12 @@ for contact in sim.detector.contacts
     calculate_weighting_potential!(sim, contact.id, refinement_limits = [0.2, 0.1, 0.05, 0.01], n_points_in_φ = 2, verbose = false)
 end
 
+wp1 = plot(sim.weighting_potentials[1])
+      plot!(sim.detector, st = :slice, φ = 0)
+wp2 = plot(sim.weighting_potentials[2])
+      plot!(sim.detector, st = :slice, φ = 0)
 plot(
-    plot(sim.weighting_potentials[1]),
-    plot(sim.weighting_potentials[2]),
+    wp1, wp2,
     size = (900, 700)
 )
 #jl savefig("tutorial_weighting_potentials.pdf") # hide
