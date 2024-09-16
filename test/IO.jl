dfd1c15a81acd6a3cf8aac84a7579a0f24fd90f3
Felix Hagemann <hagemann@mpp.mpg.de>
2024-09-16 18:28:11 +0200

    Add tests for config file read-in

--- test/IO.jl
+++ test/IO.jl
@@ -1,4 +1,22 @@
+using OrderedCollections
 using LegendHDF5IO
+
+@testset "Read in different config file formats" begin
+
+    # Create JSON file from YAML file
+    filename = SSD_examples[:InvertedCoax]
+    @test_nowarn SolidStateDetectors.yaml2json(filename)
+    
+    # Test read in using YAML, JSON and SigGen using Dict, OrderedDict and OrderedDict{String, Any}
+    for f in (filename, replace(filename, ".yaml" => ".json"), SSD_examples[:SigGen])
+        @test isfile(f)
+        @testset "$(split(f, ".")[end])" begin
+            @test_nowarn SolidStateDetectors.parse_config_file(f)
+            @test_nowarn SolidStateDetectors.parse_config_file(f, dicttype = OrderedDict)
+            @test_nowarn SolidStateDetectors.parse_config_file(f, dicttype = OrderedDict{String, Any})
+        end
+    end
+end
 
 sim = Simulation(SSD_examples[:InvertedCoax])
 
