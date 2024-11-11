595be22cf8a0c737e755a434c8a74f7186eea34e
Padniuk <ypadniuk@gmail.com>
2024-11-11 17:08:50 +0100

    Add IsotropicChargeDriftModel

--- src/SolidStateDetectors.jl
+++ src/SolidStateDetectors.jl
@@ -67,7 +67,7 @@ export Grid, GridPoint
 export ElectricPotential, PointTypes, EffectiveChargeDensity, DielectricDistribution, WeightingPotential, ElectricField
 export apply_initial_state!
 export calculate_electric_potential!, calculate_weighting_potential!, calculate_electric_field!, calculate_drift_fields!
-export ElectricFieldChargeDriftModel, ADLChargeDriftModel
+export ElectricFieldChargeDriftModel, ADLChargeDriftModel, IsotropicChargeDriftModel
 export NoChargeTrappingModel, BoggsChargeTrappingModel
 export get_active_volume, is_depleted, estimate_depletion_voltage
 export calculate_stored_energy, calculate_mutual_capacitance, calculate_capacitance_matrix
