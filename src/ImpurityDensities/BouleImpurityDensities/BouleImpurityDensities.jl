71d7ec74f81fbc32a2ed879f64e1f1bf7c1f9dca
hervasa2 <david.hervas@tum.de>
2025-04-02 15:55:50 +0200

    boule impurity docs

--- src/ImpurityDensities/BouleImpurityDensities/BouleImpurityDensities.jl
+++ src/ImpurityDensities/BouleImpurityDensities/BouleImpurityDensities.jl
@@ -1,10 +1,2 @@
-"""
-Boule impurity densities are meant to be used when the impurity density is defined in the boule coordinates, where the z-axis is aligned with the boule growth direction. 
-
-Different models are provided. In each the field `det_z0` is the z-coordinate of the detector origin in boule coordinates. The z-direction of the detector is opposite to the z-direction of the boule coordinates.
-
-In this matter the detector impurities are automatically determined from those of the boule, depending on `det_z0`.
-"""
-
 include("LinBouleImpurityDensity.jl")
 include("LinExpBouleImpurityDensity.jl")
--- src/ImpurityDensities/BouleImpurityDensities/LinExpBouleImpurityDensity.jl
+++ src/ImpurityDensities/BouleImpurityDensities/LinExpBouleImpurityDensity.jl
@@ -1,19 +1,19 @@
 """
     struct LinExpBouleImpurityDensity{T <: SSDFloat} <: AbstractImpurityDensity{T}
 
+a + b*z + c*exp((z-L)/tau)
 Impurity density model which assumes a linear + exponential component in impurity density in z.
- 
+
 ## Fields
 * `a:T`: impurity density values at the boule origin.
 * `b::T`: linear slope in `z` direction.
 * `c::T`: exponential coefficient.
 * `L::T`: exponential offset.
 * `tau::T`: exponential scale factor.
-* `det_z0::T`: z coordinate of the detector origin in boule coordinates. The z-dirrection of the detector is opposite to the z-direction of the boule coordinates.
+* `det_z0::T`: z coordinate of the detector origin in boule coordinates. The z-direction of the detector is opposite to the z-direction of the boule coordinates.
 """
 
 struct LinExpBouleImpurityDensity{T <: SSDFloat} <: AbstractImpurityDensity{T}
-    # a + b*z + c*exp((z-L)/tau) -> needs at least 4 points
     a::T
     b::T 
     c::T 
@@ -25,5 +25,5 @@ end
 function get_impurity_density(idm::LinExpBouleImpurityDensity, pt::AbstractCoordinatePoint{T})::T where {T}
     cpt = CartesianPoint(pt)
     z = cpt[3]
-    idm.a .+ idm.b * (idm.det_z0 .- z) .+ idm.c * exp.((idm.det_z0 .- z .- idm.L)/idm.tau)
+    idm.a + idm.b * (idm.det_z0 - z) + idm.c * exp((idm.det_z0 - z - idm.L)/idm.tau)
 end
