fa5f1661c3ff9dacbd079ef4aee392943ef8cea7
hervasa2 <david.hervas@tum.de>
2025-04-02 19:35:38 +0200

    boule impurity runtests

--- test/test_boule_impurity_densities.jl
+++ test/test_boule_impurity_densities.jl
@@ -21,5 +21,7 @@ T = Float32
     idm = LinExpBouleImpurityDensity{T}(boule_ρ0, boule_gradient, boule_c, boule_L, boule_tau, det_z0)
     sim.detector = SolidStateDetector(sim.detector, idm)
 
+    det_ρ0 = SolidStateDetectors.get_impurity_density(sim.detector.semiconductor.impurity_density_model, CylindricalPoint{T}(0,0,0))
+    
     @test det_ρ0 == boule_ρ0 + boule_gradient * det_z0 + boule_c * exp((det_z0 - boule_L)/boule_tau)
 end
