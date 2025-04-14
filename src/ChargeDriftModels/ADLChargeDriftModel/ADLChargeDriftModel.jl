eacba7dc9344ad0ff7fe7e72201f57388ed41f07
zpdg <zpatrick17@163.com>
2025-04-14 16:49:49 +0200

    remain the old function untouched

--- src/ChargeDriftModels/ADLChargeDriftModel/ADLChargeDriftModel.jl
+++ src/ChargeDriftModels/ADLChargeDriftModel/ADLChargeDriftModel.jl
@@ -319,7 +319,8 @@ end
     AE, RE
 end
 
-@fastmath function getVe(fv::SVector{3, T}, cdm::ADLChargeDriftModel{T,M,N}, current_pos::CartesianPoint{T} = zero(CartesianPoint{T}), Emag_threshold::T = T(1e-5))::SVector{3, T} where {T <: SSDFloat, M, N}
+getVe(fv::SVector{3, T}, cdm::ADLChargeDriftModel{T}, ::CartesianPoint{T}, Emag_threshold::T = T(1e-5)) where {T} = getVe(fv, cdm, Emag_threshold)
+@fastmath function getVe(fv::SVector{3, T}, cdm::ADLChargeDriftModel{T,M,N}, Emag_threshold::T = T(1e-5))::SVector{3, T} where {T <: SSDFloat, M, N}
     @inbounds begin
         Emag::T = norm(fv)
         Emag_inv::T = inv(Emag)
@@ -372,7 +373,8 @@ end
     p1 * x + p2 * x^2 + p3 * x^3 + p4 * x^4
 end
 
-@fastmath function getVh(fv::SVector{3,T}, cdm::ADLChargeDriftModel{T, M}, current_pos::CartesianPoint{T} = zero(CartesianPoint{T}), Emag_threshold::T = T(1e-5))::SVector{3,T} where {T <: SSDFloat, M}
+getVh(fv::SVector{3,T}, cdm::ADLChargeDriftModel{T}, ::CartesianPoint{T}, Emag_threshold::T = T(1e-5)) where {T <: SSDFloat} = getVh(fv, cdm, Emag_threshold)
+@fastmath function getVh(fv::SVector{3,T}, cdm::ADLChargeDriftModel{T, M}, Emag_threshold::T = T(1e-5))::SVector{3,T} where {T <: SSDFloat, M}
     @inbounds begin
         Emag::T = norm(fv)
         Emag_inv::T = inv(Emag)
