62da3c2acf8f4fa07207a1f714223af3adcd0ecd
Claudia Alvarez García <alvarez@cslg-04.mppmu.mpg.de>
2025-09-03 16:45:00 +0200

    pass sim.detector.semiconductor.charge_trapping_model_inactive

--- src/Simulation/Simulation.jl
+++ src/Simulation/Simulation.jl
@@ -1216,7 +1216,7 @@ function get_signal(sim::Simulation{T, CS}, drift_paths::Vector{EHDriftPath{T}},
     wpot::Interpolations.Extrapolation{T, 3} = interpolated_scalarfield(sim.weighting_potentials[contact_id])
     timestamps = _common_timestamps( drift_paths, dt )
     signal::Vector{T} = zeros(T, length(timestamps))
-    add_signal!(signal, timestamps, drift_paths, energy_depositions, wpot, sim.point_types, sim.detector.semiconductor.charge_trapping_model)
+    add_signal!(signal, timestamps, drift_paths, energy_depositions, wpot, sim.point_types, sim.detector.semiconductor.charge_trapping_model, sim.detector.semiconductor.charge_trapping_model_inactive)
     unitless_energy_to_charge = _convert_internal_energy_to_external_charge(sim.detector.semiconductor.material)
     return RDWaveform( range(zero(T) * unit(Δt), step = T(ustrip(Δt)) * unit(Δt), length = length(signal)), signal * unitless_energy_to_charge)
 end
