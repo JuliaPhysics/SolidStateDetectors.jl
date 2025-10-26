eaac2a50603731df384ffe2239456ce75f0f4ded
David Hervas Aguilar <41748838+hervasa2@users.noreply.github.com>
2025-10-26 15:17:56 +0100

    Switch `SplineBouleImpurityDensity` to `FritschCarlsonMonotonicInterpolation` (#547)
    * switch to FritschCarlsonMonotonicInterpolation
    
    * check that point is in boule
    
    * assert for SplineBouleImpurityDensity
    
    * Lower compat for Interpolations back to 0.14
    
    * Update boule impurity tests
    
    ---------
    
    Co-authored-by: Felix Hagemann <hagemann@berkeley.edu>
    (cherry picked from commit 7336b54fdb2f9184ae40496d263be88cf5f53627)

--- src/ImpurityDensities/BouleImpurityDensities/SplineBouleImpurityDensity.jl
+++ src/ImpurityDensities/BouleImpurityDensities/SplineBouleImpurityDensity.jl
@@ -1,18 +1,18 @@
 """
     struct SplineBouleImpurityDensity{N, T <: SSDFloat} <: AbstractImpurityDensity{T}
 
-Impurity density model which assumes an arbitrary impurity density in z (boule coordinates) defined by cubic spline interpolation between given points.
+Impurity density model which assumes an arbitrary impurity density in z (boule coordinates) defined by FritschCarlsonMonotonicInterpolation between given points.
 
 ## Fields
 * `z::SVector{N, T}`: z coordinates (in boule coordinates) where the impurity density is defined.
 * `ρ::SVector{N, T}`: impurity density values at the given z coordinates.
-* `spline::Interpolations.Extrapolation{T}`: cubic spline interpolation of the impurity density values.
+* `spline::Interpolations.MonotonicInterpolation{T}`: FritschCarlsonMonotonicInterpolation of the impurity density values.
 * `det_z0::T`: z coordinate of the detector origin in boule coordinates. The z-direction of the detector is opposite to the z-direction of the boule coordinates.
 """
 struct SplineBouleImpurityDensity{N, T <: SSDFloat} <: AbstractImpurityDensity{T}
     z::SVector{N, T}
     ρ::SVector{N, T}
-    spline::Interpolations.Extrapolation{T}
+    spline::Interpolations.MonotonicInterpolation{T}
     det_z0::T
 end
 
@@ -24,25 +24,12 @@ function SplineBouleImpurityDensity{T}(z::AbstractVector{<:RealQuantity}, ρ::Ab
     z = SVector{N, T}(to_internal_units.(z[idx]))
     ρ = SVector{N, T}(to_internal_units.(ρ[idx]))
 
-    dxs = diff(z)
-    z_range, resampled_ρ = if all(x -> isapprox(x, dxs[1]), dxs)
-        range(first(z), stop = last(z), length = length(z)), ρ
-    else
-        @info "Resampling impurity density to uniform steps in z"
-        min_step = minimum(dxs)
-        z_min, z_max = first(z), last(z)
-        steps = (z_max - z_min)/min_step
-        n = isapprox(ceil(steps) - steps, 1) ? Int(ceil(steps)) : Int(ceil(steps)) + 1
-        z_r = range(z_min, stop = z_max, length = n)
-        itp = Interpolations.LinearInterpolation(z, ρ, extrapolation_bc=Flat())
-        z_r, itp.(z_r)
-    end
-
-    spline = Interpolations.cubic_spline_interpolation(z_range, resampled_ρ)
+    spline = interpolate(z, ρ, FritschCarlsonMonotonicInterpolation())
     SplineBouleImpurityDensity{N, T}(z, ρ, spline, T(to_internal_units(det_z0)))
 end
 
 function get_impurity_density(idm::SplineBouleImpurityDensity, pt::AbstractCoordinatePoint{T})::T where {T}
+    @assert idm.z[begin] <= idm.det_z0 - pt.z <= idm.z[end] "SplineBouleImpurityDensity det_z0 - point.z must be within the boule's z-range."
     idm.spline(idm.det_z0 - pt.z)
 end
 
