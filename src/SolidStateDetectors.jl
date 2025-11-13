8d6268d35dbb0a5df69464375381d1c508052ba1
Claudia Alvarez Garcia <claudiaalvgar@hotmail.es>
2025-11-13 16:50:10 +0100

    delete export to_internal_point

--- src/SolidStateDetectors.jl
+++ src/SolidStateDetectors.jl
@@ -81,7 +81,7 @@ export calculate_stored_energy, calculate_mutual_capacitance, calculate_capacita
 export simulate_waveforms
 export run_geant4_simulation
 export Simulation, simulate!
-export Event, drift_charges!, to_internal_point
+export Event, drift_charges!
 export add_baseline_and_extend_tail
 export NBodyChargeCloud
 
