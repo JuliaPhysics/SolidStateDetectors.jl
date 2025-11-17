e2a5226818fd0cd1876a41dd878b77dc205666d4
hervasa2 <david.hervas@tum.de>
2025-11-17 15:22:49 +0100

    consistent ChargeDriftModel constructors

--- src/ChargeDriftModels/ElectricFieldChargeDriftModel/ElectricFieldChargeDriftModel.jl
+++ src/ChargeDriftModels/ElectricFieldChargeDriftModel/ElectricFieldChargeDriftModel.jl
@@ -6,9 +6,16 @@ Charge drift model in which the electrons and holes drift along the electric fie
 This model is the default when no charge drift model is defined in the configuration file.
 """
 struct ElectricFieldChargeDriftModel{T <: SSDFloat} <: AbstractChargeDriftModel{T} 
-    ElectricFieldChargeDriftModel{T}(::AbstractDict = Dict()) where {T <: SSDFloat} = new{T}()
 end
 
-ElectricFieldChargeDriftModel(T::Type{<:SSDFloat}) = ElectricFieldChargeDriftModel{T}()
+function ElectricFieldChargeDriftModel(; 
+        T::Type=Float32,
+        temperature::Union{Missing, Real, Unitful.Temperature} = missing
+    )::ElectricFieldChargeDriftModel{T}
+   ElectricFieldChargeDriftModel{T}()
+end
+
+ElectricFieldChargeDriftModel{T}(args...; kwargs...) where {T <: SSDFloat} = ElectricFieldChargeDriftModel(args...; T=T, kwargs...)
+
 getVe(fv::SVector{3, T}, cdm::ElectricFieldChargeDriftModel, current_pos::CartesianPoint{T} = zero(CartesianPoint{T})) where {T <: SSDFloat} = -fv
 getVh(fv::SVector{3, T}, cdm::ElectricFieldChargeDriftModel, current_pos::CartesianPoint{T} = zero(CartesianPoint{T})) where {T <: SSDFloat} = fv
--- src/ChargeDriftModels/IsotropicChargeDriftModel/IsotropicChargeDriftModel.jl
+++ src/ChargeDriftModels/IsotropicChargeDriftModel/IsotropicChargeDriftModel.jl
@@ -11,33 +11,38 @@ Charge drift model in which the electrons and holes drift along the electric fie
 struct IsotropicChargeDriftModel{T <: SSDFloat} <: AbstractChargeDriftModel{T}
     μ_e::T
     μ_h::T
-    IsotropicChargeDriftModel{T}(
-        μ_e::Union{RealQuantity, AbstractString}, 
-        μ_h::Union{RealQuantity, AbstractString}
-    ) where {T <: SSDFloat} = new{T}(_parse_value.(T, (μ_e, μ_h), internal_mobility_unit)...)
 end
 
-IsotropicChargeDriftModel(args...; T::Type = Float32, kwargs...) = IsotropicChargeDriftModel{T}(args...; kwargs...)
-
-const default_icd_config_file = joinpath(get_path_to_example_config_files(), "IsotropicChargeDriftModel/mobilities.yaml")
-
-function IsotropicChargeDriftModel{T}(config_filename::AbstractString = default_icd_config_file) where {T <: SSDFloat}
-    IsotropicChargeDriftModel(parse_config_file(config_filename))
+function _IsotropicChargeDriftModel(
+        config::AbstractDict; 
+        T::Type=Float32,
+        μ_e::Union{RealQuantity, AbstractString} = config["mobilities"]["e"], 
+        μ_h::Union{RealQuantity, AbstractString} = config["mobilities"]["h"],
+        temperature::Union{Missing, Real, Unitful.Temperature} = missing
+    )::IsotropicChargeDriftModel{T}
+   IsotropicChargeDriftModel{T}(_parse_value.(T, (μ_e, μ_h), internal_mobility_unit)...)
 end
 
-function IsotropicChargeDriftModel{T}(config::AbstractDict) where {T <: SSDFloat}
+
+# Check the syntax of the IsotropicChargeDriftModel config file before parsing
+function IsotropicChargeDriftModel(config::AbstractDict; kwargs...)
     if !haskey(config, "mobilities")
         throw(ConfigFileError("IsotropicChargeDriftModel config file needs entry 'mobilities'."))
     end
-
     for axis in ("e", "h")
         if !haskey(config["mobilities"], axis)
             throw(ConfigFileError("IsotropicChargeDriftModel config file needs entry 'mobilities/$axis'."))
         end
     end
-    
-    IsotropicChargeDriftModel{T}(config["mobilities"]["e"], config["mobilities"]["h"])
+    _IsotropicChargeDriftModel(config; kwargs...)
 end
+
+const default_icd_config_file = joinpath(get_path_to_example_config_files(), "IsotropicChargeDriftModel/mobilities.yaml")
+function IsotropicChargeDriftModel(config_filename::AbstractString = default_icd_config_file; kwargs...)
+    IsotropicChargeDriftModel(parse_config_file(config_filename); kwargs...)
+end
+
+IsotropicChargeDriftModel{T}(args...; kwargs...) where {T <: SSDFloat} = IsotropicChargeDriftModel(args...; T=T, kwargs...)
 
 calculate_mobility(cdm::IsotropicChargeDriftModel{T}, ::AbstractCoordinatePoint{T}, ::Type{Electron}) where {T <: SSDFloat} = cdm.μ_e
 calculate_mobility(cdm::IsotropicChargeDriftModel{T}, ::AbstractCoordinatePoint{T}, ::Type{Hole})     where {T <: SSDFloat} = cdm.μ_h
