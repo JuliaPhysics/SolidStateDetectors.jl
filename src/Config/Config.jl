ebb187e2512eaf63c8266c55ba7fd42fd59a2b35
Felix Hagemann <hagemann@berkeley.edu>
2025-12-11 17:11:53 -0800

    Add code to update old density configs to the new syntax

--- src/Config/Config.jl
+++ src/Config/Config.jl
@@ -26,3 +26,54 @@
 # """
 abstract type AbstractConfig{T <: SSDFloat} end
 
+function _update_density_config!(cf::AbstractDict, density::AbstractString, units::UnitTuple = default_unit_tuple())
+    
+    OLD_KEYS = Dict("linear" => ("x", "y", "z"), "cylindrical" => ("r", "z"))[cf["name"]]
+    NEW_KEYS = ("init", "gradient")
+    UNIT = Dict("impurity" => units.length^(-3), "charge" => internal_charge_unit * units.length^(-3))[density]
+
+    if any(haskey.(Ref(cf), OLD_KEYS)) # still in the old format
+        msg = """Deprecation warning: Since v0.11.0, the configuration syntax for the $(cf["name"]) $(density) density has been updated.
+        The following syntax is no longer valid (encountered unexpected fields $(filter(k -> haskey(cf, k), OLD_KEYS))):\n
+        $(YAML.write(Dict("$(density)_density" => cf)))\n"""
+
+        # convert config dictionary into the correct format
+        for k in OLD_KEYS
+            if haskey(cf, k)
+                v = pop!(cf, k)
+                # combine all inits together
+                if haskey(v, "init")
+                    value = uparse(string(v["init"]))
+                    if !iszero(value) 
+                        if !haskey(cf, "init"); cf["init"] = zero(value) end
+                        # propagate the units of the first init value with units
+                        if (value isa Quantity) && !(cf["init"] isa Quantity) cf["init"] = float(uconvert(unit(value), cf["init"] * UNIT)) end
+                        if !(value isa Quantity) && (cf["init"] isa Quantity) value = float(uconvert(unit(cf["init"]), value * UNIT)) end
+                        cf["init"] += value
+                    end
+                end
+                # rearrange the gradient, but no need to parse/add anything, so just take the value
+                if haskey(v, "gradient")
+                    value = _parse_value(Float64, v["gradient"], UNIT / internal_length_unit)
+                    if !iszero(value)
+                        if !haskey(cf, "gradient"); cf["gradient"] = OrderedCollections.OrderedDict() end
+                        cf["gradient"][k] = v["gradient"]
+                    end
+                end
+
+            end
+        end
+
+        # if there are no gradients: consider using a constant density
+        if !haskey(cf, "gradient")
+            cf["name"] = "constant"
+            cf["value"] = pop!(cf, "init", 0)
+        end
+
+        @warn msg * "Updating the config dictionary to the updated syntax:\n\n$(YAML.write(Dict("$(density)_density" => cf)))\n"
+    end
+    cf
+end
+
+@inline update_impurity_density_config!(cf::AbstractDict, units::UnitTuple) = get(cf, "name", "") in ("linear", "cylindrical") ? _update_density_config!(cf, "impurity", units) : cf
+@inline update_charge_density_config!(cf::AbstractDict, units::UnitTuple)   = get(cf, "name", "") in ("linear", "cylindrical") ? _update_density_config!(cf, "charge", units) : cf
