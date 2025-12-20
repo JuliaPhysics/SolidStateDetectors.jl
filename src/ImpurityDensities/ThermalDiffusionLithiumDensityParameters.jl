7f69bc817dc2d1ed830d5b606d65c80b8bca3058
Felix Hagemann <hagemann@berkeley.edu>
2025-12-20 10:16:46 -0800

    Store `ThermalDiffusionLithiumDensityParameters.H` in units of `cal`

--- src/ImpurityDensities/ThermalDiffusionLithiumDensityParameters.jl
+++ src/ImpurityDensities/ThermalDiffusionLithiumDensityParameters.jl
@@ -1,49 +1,41 @@
 function _ThermalDiffusionLithiumParameters(
-    config::AbstractDict;
-    T::Type = Float32,
-    temperature_ranges::AbstractVector{<:AbstractDict} = config["annealing_temperature_ranges"] ,
-    a::Union{Real, String} = config["experimental_parameters"]["a"],
-    b::Union{Real, String} = config["experimental_parameters"]["b"],
-    input_units::Union{Missing, NamedTuple} = missing
-)
+        config::AbstractDict;
+        T::Type = Float32,
+        temperature_ranges::AbstractVector{<:AbstractDict} = config["annealing_temperature_ranges"] ,
+        a::Union{Real, String} = config["experimental_parameters"]["a"],
+        b::Union{Real, String} = config["experimental_parameters"]["b"],
+        input_units::Union{Missing, NamedTuple} = missing
+    )
 
     temperature_unit = !ismissing(input_units) ? input_units.temperature : internal_temperature_unit
     diffusivity_unit = !ismissing(input_units) ?
         input_units.length^2 / internal_time_unit : internal_length_unit^2/internal_time_unit
 
-temp_ranges_parameters = Vector{LithiumDiffusionParameters{T}}()
-
-for r in temperature_ranges
-    push!(temp_ranges_parameters,
+    diffusion_params = Tuple(
         LithiumDiffusionParameters{T}(
             _parse_value(T, r["T_min"], temperature_unit),
             _parse_value(T, r["T_max"], temperature_unit),
             _parse_value(T, r["D0"], diffusivity_unit),
-            _parse_value(T, r["H"], u"cal")
+            _parse_value(T, r["H"], u"cal") / ustrip(Unitful.NoUnits, one(T) * u"cal" / internal_energy_unit) # convert to cal internally
         )
+        for r in temperature_ranges
     )
-end
 
-for i in 1:length(temp_ranges_parameters)
-    if(temp_ranges_parameters[i].T_max < temp_ranges_parameters[i].T_min)
-        throw(ConfigFileError("Invalid annealing temperature range $i: T_max must be ≥ T_min."))
+    for i in eachindex(diffusion_params)
+        if(diffusion_params[i].T_max < diffusion_params[i].T_min)
+            throw(ConfigFileError("Invalid annealing temperature range $i: T_max must be ≥ T_min."))
+        end
+        if(i>1 && diffusion_params[i-1].T_max != diffusion_params[i].T_min)
+            throw(ConfigFileError("Annealing temperature ranges must be contiguous and increasing: range $(i-1) ends at a different temperature than range $i starts."))
+        end
     end
-    if(i>1 && temp_ranges_parameters[i-1].T_max != temp_ranges_parameters[i].T_min)
-        throw(ConfigFileError("Annealing temperature ranges must be contiguous and increasing: range $i-1 ends at a different temperature than range $i starts."))
-    end
-end
 
-diffusion_params = Tuple(temp_ranges_parameters)
+    saturation_params = LithiumSaturationParameters{T}(_parse_value(T, a, Unitful.NoUnits), _parse_value(T, b, Unitful.NoUnits))
 
-saturation_params = LithiumSaturationParameters{T}(
-    T(a),
-    T(b)
-)
-
-return ThermalDiffusionLithiumDensityParameters(
-    diffusion_params,
-    saturation_params
-)
+    return ThermalDiffusionLithiumDensityParameters(
+        diffusion_params,
+        saturation_params
+    )
 end
 
 const minimum_num_of_temp_ranges = 2
