2eb3a906864c610f9850c1f158f069b1ff2fab56
gitpsii <psiiiii@proton.me>
2026-09-10 19:10:58 +0530

    Use FixedSizeVector for fixed-length waveforms

--- test/test_isochrone.jl
+++ test/test_isochrone.jl
@@ -1,10 +1,10 @@
 # This file is a part of SolidStateDetectors.jl, licensed under the MIT License (MIT).
 
 using Test
-using StaticArrays
 
 using SolidStateDetectors
 using Unitful
+using FixedSizeArrays
 
 # include("test_utils.jl")
 
@@ -31,11 +31,13 @@ time_step = T(2)u"ns"
 max_nsteps = 10000
 drift_charges!(ev, sim, Δt = time_step, max_nsteps = max_nsteps, verbose = false)
 SolidStateDetectors.get_signal!(ev, sim, 1, signal_unit = u"keV", Δt = time_step)
-@test length(add_baseline_and_extend_tail(ev.waveforms[1], 0, max_nsteps).signal) == max_nsteps
-@test add_baseline_and_extend_tail(ev.waveforms[1], 0, max_nsteps).signal isa SizedVector
+signal = add_baseline_and_extend_tail(ev.waveforms[1], 0, max_nsteps).signal
+@test length(signal) == max_nsteps
+@test signal isa FixedSizeVector
 
-@test length(add_baseline_and_extend_tail(ev.waveforms[1], max_nsteps, max_nsteps).signal) == max_nsteps
-@test add_baseline_and_extend_tail(ev.waveforms[1], max_nsteps, max_nsteps).signal isa SizedVector
+signal = add_baseline_and_extend_tail(ev.waveforms[1], max_nsteps, max_nsteps).signal
+@test length(signal) == max_nsteps
+@test signal isa FixedSizeVector
 
 DT_h = Array{typeof(time_step),2}(fill(typeof(time_step)(NaN),length(x_axis),length(z_axis)))
 DT_e = Array{typeof(time_step),2}(fill(typeof(time_step)(NaN),length(x_axis),length(z_axis)))
