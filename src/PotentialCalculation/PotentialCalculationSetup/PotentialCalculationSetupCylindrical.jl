d9740c6c4838f118e3f5298618aa24a8ec1da58f
Claudia Alvarez Garcia <claudiaalvgar@hotmail.es>
2026-09-11 13:44:16 +0200

    Shifts gauge reference

--- src/PotentialCalculation/PotentialCalculationSetup/PotentialCalculationSetupCylindrical.jl
+++ src/PotentialCalculation/PotentialCalculationSetup/PotentialCalculationSetupCylindrical.jl
@@ -452,7 +452,7 @@ function ElectricPotentialArray(pcs::PotentialCalculationSetup{T, Cylindrical, 3
         p_r0 = mean(pot[1,:,iz])
         pot[1,:,iz] .= p_r0
     end
-    return pot
+    return pot .+ pcs.gauge_ref_potential
 end
 
 function ImpurityScaleArray(pcs::PotentialCalculationSetup{T, Cylindrical, 3, Array{T, 3}})::Array{T, 3} where {T}
